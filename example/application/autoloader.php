f78a9fc510a90a16a78ecd5e11076b8b7f76b648
bravedave <davbray@gmail.com>
2017-04-07 16:27:49 +1000

    All about Autoloader

--- example/application/autoloader.php
+++ example/application/autoloader.php
@@ -6,23 +6,13 @@
 
 	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
 		http://creativecommons.org/licenses/by/4.0/
-
-	This is basically the system, when a class is not found, it routes through here
-	/app/ is checked before /lib/ which means that classes in lib can be overwritten
-
 	*/
 
 spl_autoload_register(function ($class) {
-	if ( $lib = realpath( __DIR__ . '/../app/' . str_replace('\\', '/', $class) . '.php')) {
+	if ( $lib = realpath( __DIR__ . '/app/' . str_replace('\\', '/', $class) . '.php')) {
 		include_once $lib;
 		dvc\sys::logloader( sprintf( 'app: %s', $lib ));
 
-		return ( TRUE);
-
-	}
-	elseif ( $lib = realpath( __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php')) {
-		include_once $lib;
-		dvc\sys::logloader( sprintf( 'lib: %s', $lib ));
 		return ( TRUE);
 
 	}
@@ -31,4 +21,4 @@ spl_autoload_register(function ($class) {
 
 });
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bravedave/dvc/autoloader.php';
