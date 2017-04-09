3ceb58acd2ee2cb3014ef3a43049ec57513ff7c9
Dingo Star <david@brayworth.com.au>
2017-04-09 14:39:37 +1000

    Tweak

--- url.php
+++ url.php
@@ -7,6 +7,7 @@
 	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
 		http://creativecommons.org/licenses/by/4.0/
 
+	DO NOT change this file
+	Copy it to <application>/app and modify it there
 	*/
-
 class url extends dvc\url {}
