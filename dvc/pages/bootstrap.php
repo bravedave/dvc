6096ff3781334d28fe64454606ad1e0aae5ed626
bravedave <davbray@gmail.com>
2017-04-18 14:54:38 +1000

    Added Bootstrap template

---
+++ dvc/pages/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+/*
+	David Bray
+	BrayWorth Pty Ltd
+	e. david@brayworth.com.au
+
+	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
+		http://creativecommons.org/licenses/by/4.0/
+
+	DO NOT change this file
+	Copy it to <application>/app/dvc/ and modify it there
+	*/
+NameSpace dvc;
+
+class bootstrap extends _page {
+	function __construct( $title = '' ) {
+		parent::__construct( $title );
+
+		array_unshift( $this->css, sprintf( '<link type="text/css" rel="stylesheet" media="all" href="%s" />', url::$URL . 'css/bootstrap.min.css' ));
+		$this->latescripts[] = sprintf( '<script type="text/javascript" src="%s"></script>', url::$URL . 'js/bootstrap.min.js' );
+
+	}
+
+	public function title( $navbar =  'navbar-inverse') {
+		parent::title( $navbar);
+		return ( $this);
+
+	}
+
+}
