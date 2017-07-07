417472d3d0f787168f89f6b0976aa285fb3badd1
bravedave <davbray@gmail.com>
2017-07-07 11:03:33 +1000

    Adjusted for WindowScrollTop

--- dvc/public/js/_brayworth_.context.js
+++ dvc/public/js/_brayworth_.context.js
@@ -85,9 +85,9 @@ _brayworth_.context = function() {
 			}
 
 			/* try to keep menu on screen vertically */
-			if ( offset.top + this.root.height() > $(window).height()) {
+			if ( offset.top + this.root.height() > ( $(window).height()+$(window).scrollTop())) {
 				//~ console.log( 'uh oh - top!');
-				var t = $(window).height()-this.root.height()-5;
+				var t = ($(window).height()+$(window).scrollTop())-this.root.height()-5;
 				this.root.css( 'top', Math.max( t, $(window).scrollTop()+2));
 				offset = this.root.offset();
 
