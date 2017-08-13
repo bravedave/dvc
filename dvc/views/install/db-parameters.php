31540fb1004b765ab943a39c199b271ed80993dc
Dingo Star <david@brayworth.com.au>
2017-08-13 22:32:36 +1000

    DB Config and Install

--- dvc/views/install/db-parameters.php
+++ dvc/views/install/db-parameters.php
@@ -9,60 +9,60 @@
 
 	*/
   if ( ! defined( "APPLICATION")) exit; ?>
-<form action="<?php print url::$URL ?>install/db" method="POST">
-  <fieldset>
-    <legend>DB Parameters</legend>
+<form action="<?php print url::tostring('install/') ?>" method="POST">
+	<fieldset>
+		<legend>DB Parameters</legend>
 
-    <div class="form-group row">
-      <label class="control-label col-sm-3" for="db_host">MySQL Host</label>
-      <div class="col-sm-9">
-        <input class="form-control " type="text" name="db_host" id="db_host" value="localhost" autofocus />
+		<div class="form-group row">
+			<label class="control-label col-sm-3" for="db_host">MySQL Host</label>
+			<div class="col-sm-9">
+				<input class="form-control " type="text" name="db_host" id="db_host" value="localhost" autofocus />
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-    <div class="form-group row">
-      <label class="control-label col-sm-3" for="root_password">MySQL Root Password</label>
-      <div class="col-sm-9">
-        <input class="form-control " type="password" name="root_password" id="root_password" />
+		<div class="form-group row">
+			<label class="control-label col-sm-3" for="root_password">MySQL Root Password</label>
+			<div class="col-sm-9">
+				<input class="form-control " type="password" name="root_password" id="root_password" />
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-    <div class="form-group row">
-      <label class="control-label col-sm-3" for="db_name">Database Name</label>
-      <div class="col-sm-9">
-        <input class="form-control " type="text" name="db_name" id="db_name" />
+		<div class="form-group row">
+			<label class="control-label col-sm-3" for="db_name">Database Name</label>
+			<div class="col-sm-9">
+				<input class="form-control " type="text" name="db_name" id="db_name" />
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-    <div class="form-group row">
-      <label class="control-label col-sm-3" for="db_user">Database User</label>
-      <div class="col-sm-9">
-        <input class="form-control " type="text" name="db_user" id="db_user" />
+		<div class="form-group row">
+			<label class="control-label col-sm-3" for="db_user">Database User</label>
+			<div class="col-sm-9">
+				<input class="form-control " type="text" name="db_user" id="db_user" />
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-    <div class="form-group row">
-      <label class="control-label col-sm-3" for="db_pass">Database Password</label>
-      <div class="col-sm-9">
-        <input class="form-control " type="text" name="db_pass" id="db_pass" />
+		<div class="form-group row">
+			<label class="control-label col-sm-3" for="db_pass">Database Password</label>
+			<div class="col-sm-9">
+				<input class="form-control " type="text" name="db_pass" id="db_pass" />
 
-      </div>
+			</div>
 
-    </div>
+		</div>
 
-    <div class="form-group text-right">
-      <input class="btn btn-default" name="form_action" id="form_action" type="submit" value="Submit" />
+		<div class="form-group text-right">
+			<input class="btn btn-default" name="form_action" id="form_action" type="submit" value="Submit" />
 
-    </div>
+		</div>
 
-  </fieldset>
+	</fieldset>
 
 </form>
