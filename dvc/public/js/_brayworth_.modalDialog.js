5fe35621e97aa7afdcc4e751ac092fdbae503adf
bravedave <davbray@gmail.com>
2017-12-08 11:48:38 +1000

    add a (anchor) to list of autofocus elements

--- dvc/public/js/_brayworth_.modalDialog.js
+++ dvc/public/js/_brayworth_.modalDialog.js
@@ -56,7 +56,7 @@ $.fn.modalDialog = _brayworth_.modalDialog = function ( _options) {
 
 	}
 	else {
-		_AF = $('textarea, input, button',modal);
+		_AF = $('textarea, input, button, a',modal);
 		if ( _AF.length > 0)
 			_AF.first().focus();
 
