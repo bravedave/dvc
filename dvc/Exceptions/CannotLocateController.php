d29879bff50ea0377ab6e8335781019ca11c9398
Dave Bray <david@brayworth.com.au>
2017-12-22 18:43:44 +1000

    added exception

---
+++ dvc/Exceptions/CannotLocateController.php
@@ -0,0 +1,16 @@
+<?php
+/*
+	David Bray
+	BrayWorth Pty Ltd
+	e. david@brayworth.com.au
+
+	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
+		http://creativecommons.org/licenses/by/4.0/
+
+	*/
+NameSpace dvc\Exceptions;
+
+class CannotLocateController extends Exception {
+	protected $_text = 'cannot locate the controller file';
+
+}
