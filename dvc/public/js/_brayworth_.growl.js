87d809557530503dcbdbfc55c384e3b5499e0bf8
Dave Bray <david@brayworth.com.au>
2018-03-10 07:19:31 +1000

    Growler returns a promise which resoves when the timeout expires

--- dvc/public/js/_brayworth_.growl.js
+++ dvc/public/js/_brayworth_.growl.js
@@ -30,7 +30,7 @@
 		else if ( /string/i.test( typeof params ))
 			options.text = params;
 
-		_brayworth_.growl.call( this, options);
+		return ( _brayworth_.growl.call( this, options));
 
 	}
 
@@ -42,7 +42,7 @@
 		else if ( /string/i.test( typeof params ))
 			options.text = params;
 
-		_brayworth_.growl.call( this, options);
+		return ( _brayworth_.growl.call( this, options));
 
 	}
 
@@ -67,124 +67,138 @@
 		if ( !!j.timeout)
 			options.timeout = j.timeout;
 
-		_brayworth_.growl.call( this, options);
+		return ( _brayworth_.growl.call( this, options));
 
 	}
 
 	var growlers = [];
 	_brayworth_.growl = function( params) {
+
 		var host = ( this == _brayworth_ ? $('body') : this)
-		if ( 'string' == typeof this)
+		if ( 'string' == typeof this) {
 			host = $(host);
-		else if ( this instanceof String)
+
+		}
+		else if ( this instanceof String) {
 			host = $(host.valueOf());
-		else if ( 'object' == typeof this  && !!this.xhr)
+
+		}
+		else if ( 'object' == typeof this  && !!this.xhr) {
 			host = $('body');
-		else if ( !( this instanceof jQuery))
+
+		}
+		else if ( !( this instanceof jQuery)) {
 			host = $(host);
 
-		//~ console.log( typeof this, this instanceof String);
-		//~ console.log( host);
-
-		var options = {
-			top : 60,
-			right : 20,
-			text : '',
-			title : '',
-			timeout : 2000,
-			growlClass : 'information',
-
 		}
 
-		if ( 'object' == typeof params)
-			$.extend( options, params);
-		else if ( 'string' == typeof params )
-			options.text = params;
+		return new Promise( function( resolve, reject) {
 
-		if ( options.title == '' && options.text == '') {
-			// this is a little repetitive - it's an ajax response
-			if ( !!params.response)
-				options.growlClass = ( params.response == 'ack' ? 'success' : 'error' );
+			//~ console.log( typeof this, this instanceof String);
+			//~ console.log( host);
 
-			if ( !!params.description)
-				options.text = params.description;
-
-			if ( !!params.timeout)
-				options.timeout = params.timeout;
-			else if ( options.growlClass == 'error')
-				options.timeout = 5000;
-
-		}
-
-		if ( options.title == '' && options.text == '')
-			return;	// abandon ship
-
-		var growler = $('<div class="growler" />');
-
-		/*
-		 * you have to find a place in the growlers for this one
-		 */
-		var growlerIndex = -1
-		$.each( growlers, function( i, e) {
-			if ( !e) {
-				growlerIndex = i;
-				growlers[growlerIndex] = growler;
-				//~ console.log( 'growlerIndex - recycle', growlerIndex);
-				return ( false);
+			var options = {
+				top : 60,
+				right : 20,
+				text : '',
+				title : '',
+				timeout : 2000,
+				growlClass : 'information',
 
 			}
+
+			if ( 'object' == typeof params)
+				$.extend( options, params);
+			else if ( 'string' == typeof params )
+				options.text = params;
+
+			if ( options.title == '' && options.text == '') {
+				// a little repetitive - it's an ajax response
+				if ( !!params.response)
+					options.growlClass = ( params.response == 'ack' ? 'success' : 'error' );
+
+				if ( !!params.description)
+					options.text = params.description;
+
+				if ( !!params.timeout)
+					options.timeout = params.timeout;
+				else if ( options.growlClass == 'error')
+					options.timeout = 5000;
+
+			}
+
+			if ( options.title == '' && options.text == '')
+				return;	// abandon ship
+
+			var growler = $('<div class="growler" />');
+
+			/*
+			 * you have to find a place in the growlers for this one
+			 */
+			var growlerIndex = -1
+			$.each( growlers, function( i, e) {
+				if ( !e) {
+					growlerIndex = i;
+					growlers[growlerIndex] = growler;
+					//~ console.log( 'growlerIndex - recycle', growlerIndex);
+					return ( false);
+
+				}
+
+			});
+
+			if ( growlerIndex < 0) {
+				// grow the index
+				growlerIndex = growlers.length;
+				growlers[growlerIndex] = growler;
+				//~ console.log( 'growlerIndex - new', growlerIndex);
+
+			}
+
+			if ( host[0].tagName == 'BODY' || host.css('position') != 'static') {
+				options.top *= growlerIndex;	// growler is offset down screen to avoid stacking
+			}
+			else {
+				try {
+					var offset = host.offset();
+					options.top = offset.top - 20;
+					options.right = Math.min( $(window).width(), offset.left + host.width() + 20);
+
+					//~ console.log( options.top, options.right);
+
+				}
+				catch (e) {
+					console.warn( host, e);
+
+				}
+
+			}
+
+			options.top = Math.max( options.top, $(window).scrollTop());
+
+			var title = $('<h3 />');
+			var content = $('<div />');
+
+			if ( options.title != '')
+				title.html(options.title).appendTo( growler);
+			else
+				content.css('padding-top','5px');
+
+			if ( options.text != '')
+				content.html(options.text).appendTo( growler);
+
+			growler
+				.css({ 'position' : 'absolute', 'top' : options.top, 'right' : options.right })
+				.addClass(options.growlClass)
+				.appendTo( host);
+
+			setTimeout( function() {
+				growlers[growlerIndex] = false;
+				growler.remove();
+				resolve();
+			}, options.timeout)
 
 		});
-
-		if ( growlerIndex < 0) {
-			// grow the index
-			growlerIndex = growlers.length;
-			growlers[growlerIndex] = growler;
-			//~ console.log( 'growlerIndex - new', growlerIndex);
-
-		}
-
-		if ( host[0].tagName == 'BODY' || host.css('position') != 'static') {
-			options.top *= growlerIndex;	// this growler is offset down screen to avoid stacking
-		}
-		else {
-			try {
-				var offset = host.offset();
-				options.top = offset.top - 20;
-				options.right = Math.min( $(window).width(), offset.left + host.width() + 20);
-
-				//~ console.log( options.top, options.right);
-
-			}
-			catch (e) {
-				console.warn( host, e);
-
-			}
-
-		}
-
-		options.top = Math.max( options.top, $(window).scrollTop());
-
-		var title = $('<h3 />');
-		var content = $('<div />');
-
-		if ( options.title != '')
-			title.html(options.title).appendTo( growler);
-		else
-			content.css('padding-top','5px');
-
-		if ( options.text != '')
-			content.html(options.text).appendTo( growler);
-
-		growler
-			.css({ 'position' : 'absolute', 'top' : options.top, 'right' : options.right })
-			.addClass(options.growlClass)
-			.appendTo( host);
-
-		setTimeout( function() {
-			growlers[growlerIndex] = false;
-			growler.remove();
-		}, options.timeout)
 
 	}
 
