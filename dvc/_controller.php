cbba26f0b2b3be6d0aad7c4ba6f394aeac0c3c32
Dave Bray <david@brayworth.com.au>
2018-04-05 07:24:13 +1000

    added option to pass data to the page

--- dvc/_controller.php
+++ dvc/_controller.php
@@ -417,12 +417,14 @@ abstract class _controller {
 			'title' => $this->title,
 			'scripts' => [],
 			'css' => [],
+			'data' => FALSE,
 
 		];
 
 		$options = array_merge( $defaults, $params);
 
 		$p = new $options['template']( $options['title']);
+		$p->data = $options->data;
 
 		foreach ( $options['scripts'] as $script) {
 			$p->scripts[] = sprintf( '<script type="text/javascript" src="%s"></script>', $script );
