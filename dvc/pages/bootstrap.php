0913c260e21fecb6ac89e80d79b4ebf57a97d656
Dave Bray <david@brayworth.com.au>
2018-04-05 14:15:03 +1000

    remove dvc templates just use bootstrap

--- dvc/pages/bootstrap.php
+++ dvc/pages/bootstrap.php
@@ -17,8 +17,8 @@ class bootstrap extends page {
 
 	function __construct( $title = '' ) {
 		self::$BootStrap = TRUE;
-		if ( self::$BootStrap_Version == '4' && $this->dvc)
-			$this->dvc = '4';
+		if ( self::$BootStrap_Version == '4')
+			$this->dvc = FALSE;
 
 		parent::__construct( $title );
 
