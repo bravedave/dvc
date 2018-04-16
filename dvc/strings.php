75e24d90c80aa5d4d259a72bdd69c63bd38f369c
Dave Bray <david@brayworth.com.au>
2018-04-17 08:59:43 +1000

    added shortdate converter

--- dvc/strings.php
+++ dvc/strings.php
@@ -10,19 +10,117 @@
 NameSpace dvc;
 
 abstract class strings {
+	static function asShortDate( $date, $time = FALSE) {
+		if ( (string)$date == '0000-00-00')
+		return ( FALSE);
+
+		if ( ( $t = strtotime( $date)) !== FALSE) {
+			if ( $time && date( 'Y-m-d', $t) == date( 'Y-m-d'))
+			return ( date( 'g:ia', $t));
+			elseif ( date( 'Y', $t) == date( 'Y'))
+			return ( date( 'd-M', $t));
+			else
+			return ( date( config::$DATE_FORMAT, $t));
+
+		}
+
+		return FALSE;
+
+	}
+
+	static function array2csv(array &$array) {
+		if (count($array) == 0)
+		return null;
+
+		ob_start();
+		$df = fopen("php://output", 'w');
+		//~ fputcsv( $df, array_keys( reset( $array)));
+		foreach ( $array as $row)
+		fputcsv($df, $row);
+
+		fclose($df);
+
+		return ob_get_clean();
+
+	}
+
+	static function BRITISHDateAsANSI( $strDate) {
+		// split it, must have 3 parts, dd/mm/yyyy
+		$a = explode( "/", $strDate );
+		if ( @checkdate( $a[1], $a[0], $a[2] ))
+		return $a[2] . "-" . str_pad( $a[1], 2, "0", STR_PAD_LEFT ) . "-" . str_pad( $a[0], 2, "0", STR_PAD_LEFT );
+
+		return 0;
+
+	}
+
+	static function CheckEmailAddress( $email) {
+		return ( filter_var($email, FILTER_VALIDATE_EMAIL));
+
+	}
+
+	static function getCommonPath( $paths) {
+		$lastOffset = 1;
+		$common = '/';
+		while (($index = strpos($paths[0], '/', $lastOffset)) !== FALSE) {
+			$dirLen = $index - $lastOffset + 1;	// include /
+			$dir = substr($paths[0], $lastOffset, $dirLen);
+			foreach ($paths as $path) {
+				if (substr($path, $lastOffset, $dirLen) != $dir)
+				return $common;
+			}
+			$common .= $dir;
+			$lastOffset = $index + 1;
+
+		}
+		return substr($common, 0, -1);
+
+	}
+
+	static function getDateAsANSI( $strDate) {
+
+		if ( preg_match("@^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$@", $strDate))
+		return ( date( 'Y-m-d', strtotime( $strDate)));
+
+		if ( config::$DATE_FORMAT == 'd/m/Y' )
+		return ( self::BRITISHDateAsANSI($strDate));
+
+
+		throw new Exception( 'fix me:: strings::getDateAsANSI' );
+
+	}
+
+	static function initials( $name ) {
+		if ( (string)$name == "" )
+		return '';
+
+		$aX = explode( " ", $name );
+		$xX = "";
+		foreach ( $aX as $sX ) {
+			$xX .= substr( $sX, 0, 1 );
+
+		}
+
+		return ($xX );
+
+	}
+
+	static function InLocalTimeZone($format="r", $timestamp=false, $timezone=false) {
+		$userTimezone = new \DateTimeZone( !empty($timezone) ? $timezone : 'GMT');
+		$gmtTimezone = new \DateTimeZone( 'GMT');
+		$myDateTime = new \DateTime( ( $timestamp != false ? date("r", (int)$timestamp) : date("r")), $gmtTimezone);
+		$offset = $userTimezone->getOffset( $myDateTime);
+		return date( $format, ($timestamp!=false ? (int)$timestamp : $myDateTime->format('U')) + $offset);
+
+	}
+
 	static function isValidMd5($md5 ='') {
 	    return preg_match('/^[a-f0-9]{32}$/', $md5);
 
 	}
 
 	static function IsEmailAddress( $email) {
-		//~ return ( filter_var($email, FILTER_VALIDATE_EMAIL));
 		return ( self::CheckEmailAddress($email));
-
-	}
-
-	static function CheckEmailAddress( $email) {
-		return ( filter_var($email, FILTER_VALIDATE_EMAIL));
 
 	}
 
@@ -40,86 +138,8 @@ abstract class strings {
 
 	}
 
-	static function initials( $name ) {
-		if ( (string)$name == "" )
-			return '';
-
-		$aX = explode( " ", $name );
-		$xX = "";
-		foreach ( $aX as $sX )
-			$xX .= substr( $sX, 0, 1 );
-
-		return ($xX );
-	}
-
 	static function lorum() {
 		return html\element::lorum();
-
-	}
-
-	static function BRITISHDateAsANSI( $strDate) {
-		// split it, must have 3 parts, dd/mm/yyyy
-		$a = explode( "/", $strDate );
-		if ( @checkdate( $a[1], $a[0], $a[2] ))
-			return $a[2] . "-" . str_pad( $a[1], 2, "0", STR_PAD_LEFT ) . "-" . str_pad( $a[0], 2, "0", STR_PAD_LEFT );
-
-		return 0;
-
-	}
-
-	static function InLocalTimeZone($format="r", $timestamp=false, $timezone=false) {
-		$userTimezone = new \DateTimeZone( !empty($timezone) ? $timezone : 'GMT');
-		$gmtTimezone = new \DateTimeZone( 'GMT');
-		$myDateTime = new \DateTime( ( $timestamp != false ? date("r", (int)$timestamp) : date("r")), $gmtTimezone);
-		$offset = $userTimezone->getOffset( $myDateTime);
-		return date( $format, ($timestamp!=false ? (int)$timestamp : $myDateTime->format('U')) + $offset);
-
-	}
-
-	static function asShortDate( $date) {
-		if ( (string)$date == '0000-00-00')
-			return ( FALSE);
-
-		if ( ( $t = strtotime( $date)) !== FALSE) {
-			if ( date( 'Y', $t) == date( 'Y'))
-				return ( date( 'd-M', $t));
-			else
-				return ( date( config::$DATE_FORMAT, $t));
-
-		}
-
-		return FALSE;
-
-	}
-
-	static function getDateAsANSI( $strDate) {
-
-		if ( preg_match("@^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$@", $strDate))
-			return ( date( 'Y-m-d', strtotime( $strDate)));
-
-		if ( config::$DATE_FORMAT == 'd/m/Y' )
-			return ( self::BRITISHDateAsANSI($strDate));
-
-
-		throw new Exception( 'fix me:: strings::getDateAsANSI' );
-
-	}
-
-	static function getCommonPath( $paths) {
-		$lastOffset = 1;
-		$common = '/';
-		while (($index = strpos($paths[0], '/', $lastOffset)) !== FALSE) {
-			$dirLen = $index - $lastOffset + 1;	// include /
-			$dir = substr($paths[0], $lastOffset, $dirLen);
-			foreach ($paths as $path) {
-				if (substr($path, $lastOffset, $dirLen) != $dir)
-					return $common;
-			}
-			$common .= $dir;
-			$lastOffset = $index + 1;
-
-		}
-		return substr($common, 0, -1);
 
 	}
 
@@ -138,22 +158,6 @@ abstract class strings {
 		$testlen = strlen($test);
 		if ($testlen > $strlen) return false;
 		return substr_compare( $string, $test, $strlen - $testlen, $testlen, TRUE) === 0;
-
-	}
-
-	static function array2csv(array &$array) {
-		if (count($array) == 0)
-			return null;
-
-		ob_start();
-		$df = fopen("php://output", 'w');
-		//~ fputcsv( $df, array_keys( reset( $array)));
-		foreach ( $array as $row)
-			fputcsv($df, $row);
-
-		fclose($df);
-
-		return ob_get_clean();
 
 	}
 
