0ae3c68f3d004f59fe83c997ac8f817bdc3e365d
Dave Bray <david@brayworth.com.au>
2018-05-26 07:51:16 +1200

    added bootstrap 4 alert when timeout is 0

--- dvc/public/js/_brayworth_.growl.js
+++ dvc/public/js/_brayworth_.growl.js
@@ -48,7 +48,7 @@
 
 	_brayworth_.growlAjax = function( j) {
 		/*
-			my standard ajax response is {
+			standard ajax response is {
 				response : 'ack or nak',
 				description : 'blah blah ..'
 			}
@@ -107,96 +107,136 @@
 
 			}
 
-			if ( 'object' == typeof params)
+			if ( 'object' == typeof params) {
 				$.extend( options, params);
-			else if ( 'string' == typeof params )
+
+			}
+			else if ( 'string' == typeof params ) {
 				options.text = params;
 
-			if ( options.title == '' && options.text == '') {
-				// a little repetitive - it's an ajax response
+			}
+
+			if ( options.title == '' || options.text == '') {
+				/*
+				* a little repetitive - it's an ajax response
+				*
+				* the basic ajax response is:
+				* { response : 'ack', description : 'go you good thing' }
+				*
+				* with an optional timeout set to 0 it will become a bootstrap 4valert:
+				* { response : 'ack', description : 'go you good thing', timeout : 0 }
+				*/
 				if ( !!params.response)
 					options.growlClass = ( params.response == 'ack' ? 'success' : 'error' );
 
 				if ( !!params.description)
 					options.text = params.description;
 
-				if ( !!params.timeout)
-					options.timeout = params.timeout;
-				else if ( options.growlClass == 'error')
-					options.timeout = 5000;
+			}
+
+			if ( !!params.timeout) {
+				options.timeout = params.timeout;
+
+			}
+			else if ( options.growlClass == 'error') {
+				options.timeout = 5000;
 
 			}
 
 			if ( options.title == '' && options.text == '')
 				return;	// abandon ship
 
-			var growler = $('<div class="growler" />');
+			if ( 0 == options.timeout) {
+				/*
+				* with an optional timeout set to 0 it will become a bootstrap 4valert:
+				* { response : 'ack', description : 'go you good thing', timeout : 0 }
+				*/
+				let growler = $('<div class="alert alert-warning alert-dismissible fade show m-1" role="alert" />');
 
-			/*
-			 * you have to find a place in the growlers for this one
-			 */
-			var growlerIndex = -1
-			$.each( growlers, function( i, e) {
-				if ( !e) {
-					growlerIndex = i;
-					growlers[growlerIndex] = growler;
-					//~ console.log( 'growlerIndex - recycle', growlerIndex);
-					return ( false);
+				if ( options.title != '') {
+					let title = $('<h3 />').html(options.title).appendTo( growler);
 
 				}
 
-			});
+				$('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>').appendTo(growler);
 
-			if ( growlerIndex < 0) {
-				// grow the index
-				growlerIndex = growlers.length;
-				growlers[growlerIndex] = growler;
-				//~ console.log( 'growlerIndex - new', growlerIndex);
+				if ( options.text != '') {
+					let content = $('<div />').html(options.text).appendTo( growler);
 
-			}
+				}
 
-			if ( host[0].tagName == 'BODY' || host.css('position') != 'static') {
-				options.top *= growlerIndex;	// growler is offset down screen to avoid stacking
+				growler.prependTo('body');
+
 			}
 			else {
-				try {
-					var offset = host.offset();
-					options.top = offset.top - 20;
-					options.right = Math.min( $(window).width(), offset.left + host.width() + 20);
+				let growler = $('<div class="growler" />');
 
-					//~ console.log( options.top, options.right);
+				/*
+				* you have to find a place in the growlers for this one
+				*/
+				let growlerIndex = -1
+				$.each( growlers, function( i, e) {
+					if ( !e) {
+						growlerIndex = i;
+						growlers[growlerIndex] = growler;
+						//~ console.log( 'growlerIndex - recycle', growlerIndex);
+						return ( false);
+
+					}
+
+				});
+
+				if ( growlerIndex < 0) {
+					// grow the index
+					growlerIndex = growlers.length;
+					growlers[growlerIndex] = growler;
+					//~ console.log( 'growlerIndex - new', growlerIndex);
 
 				}
-				catch (e) {
-					console.warn( host, e);
+
+				if ( host[0].tagName == 'BODY' || host.css('position') != 'static') {
+					options.top *= growlerIndex;	// growler is offset down screen to avoid stacking
+				}
+				else {
+					try {
+						var offset = host.offset();
+						options.top = offset.top - 20;
+						options.right = Math.min( $(window).width(), offset.left + host.width() + 20);
+
+						//~ console.log( options.top, options.right);
+
+					}
+					catch (e) {
+						console.warn( host, e);
+
+					}
 
 				}
 
-			}
+				options.top = Math.max( options.top, $(window).scrollTop());
 
-			options.top = Math.max( options.top, $(window).scrollTop());
+				var title = $('<h3 />');
+				var content = $('<div />');
 
-			var title = $('<h3 />');
-			var content = $('<div />');
+				options.title != '' ?
+					title.html(options.title).appendTo( growler) :
+					content.css('padding-top','5px');
 
-			if ( options.title != '')
-				title.html(options.title).appendTo( growler);
-			else
-				content.css('padding-top','5px');
+				if ( options.text != '')
+					content.html(options.text).appendTo( growler);
 
-			if ( options.text != '')
-				content.html(options.text).appendTo( growler);
-
-			growler
+				growler
 				.css({ 'position' : 'absolute', 'top' : options.top, 'right' : options.right })
 				.addClass(options.growlClass)
 				.appendTo( host);
 
-			setTimeout( function() {
-				growlers[growlerIndex] = false;
-				growler.remove();
-				resolve();
-			}, options.timeout)
+				setTimeout( function() {
+					growlers[growlerIndex] = false;
+					growler.remove();
+					resolve();
+				}, options.timeout)
+
+			}
 
 		});
 
