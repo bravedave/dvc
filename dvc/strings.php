e9479ee72d23b34dd9b93ef4bdd798e560e60484
Dave Bray <david@brayworth.com.au>
2018-06-20 16:07:33 +1000

    uid/guid

--- dvc/strings.php
+++ dvc/strings.php
@@ -100,20 +100,23 @@ abstract class strings {
 
 	}
 
-	static function getGUID(){
-    $charid = strtoupper(md5(uniqid(rand(), true)));
-    $hyphen = chr(45);  // "-"
-    $uuid = chr(123)    // "{"
-      .substr($charid, 0, 8).$hyphen
-      .substr($charid, 8, 4).$hyphen
-      .substr($charid,12, 4).$hyphen
-      .substr($charid,16, 4).$hyphen
-      .substr($charid,20,12)
-      .chr(125);        // "}"
+	static function getGUID() {
+		return ( sprint( '{%s}', self::getUID()));
 
-    return $uuid;
+	}
 
-  }
+	static function getUID(){
+		$charid = strtoupper(md5(uniqid(rand(), true)));
+		$uuid = sprintf( '%s-%s-%s-%s-%s',
+			substr($charid, 0, 8),
+			substr($charid, 8, 4),
+			substr($charid,12, 4),
+			substr($charid,16, 4),
+			substr($charid,20,12));
+
+		return $uuid;
+
+	}
 
 	static function initials( $name ) {
 		if ( (string)$name == "" )
