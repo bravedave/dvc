9c768cdd5b0712057a96c6891f397ebf9ceaf969
Dave Bray <david@brayworth.com.au>
2018-06-20 15:53:28 +1000

    added guid generator

--- dvc/strings.php
+++ dvc/strings.php
@@ -100,6 +100,21 @@ abstract class strings {
 
 	}
 
+	static function getGUID(){
+    $charid = strtoupper(md5(uniqid(rand(), true)));
+    $hyphen = chr(45);  // "-"
+    $uuid = chr(123)    // "{"
+      .substr($charid, 0, 8).$hyphen
+      .substr($charid, 8, 4).$hyphen
+      .substr($charid,12, 4).$hyphen
+      .substr($charid,16, 4).$hyphen
+      .substr($charid,20,12)
+      .chr(125);        // "}"
+
+    return $uuid;
+
+  }
+
 	static function initials( $name ) {
 		if ( (string)$name == "" )
 		return '';
