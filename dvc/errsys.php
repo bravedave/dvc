f882fffa01f1ba06133177171e3b31a383ac81db
Dave Bray <david@brayworth.com.au>
2018-07-20 11:27:26 +1000

    added extra tracing of random unkown error

--- dvc/errsys.php
+++ dvc/errsys.php
@@ -86,9 +86,14 @@ abstract class errsys {
 			/*
 			 * error is logged in the exception
 			 */
-			error_log( '---[probable duplicate    : error is logged in the exception]---');
-			error_log( sprintf( '%s: %s %s %s %s', $type, $errstr, $errno, $errfile, $errline));
-			error_log( '---[end probable duplicate: error is logged in the exception]---');
+			if ( $errstr == 'Unknown') {
+				error_log( '---[probable duplicate    : error is logged in the exception]---');
+				error_log( sprintf( '%s: %s %s %s %s', $type, $errstr, $errno, $errfile, $errline));
+				\sys::trace($errno);
+				error_log( '---[end probable duplicate: error is logged in the exception]---');
+
+			}
+
 			$exception = new \Exception( sprintf( '%s: %s %s %s %s %s', $type, $errstr, $errno, PHP_EOL, $errfile, $errline));
 
 			if ( $exit ) {
