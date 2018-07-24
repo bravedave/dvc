cbf7df53406abf00dbd2add062b77aea074a9d25
Dave Bray <david@brayworth.com.au>
2018-07-25 08:48:42 +1000

    include a local autoloader

--- autoloader-local.php
+++ autoloader-local.php
@@ -7,7 +7,6 @@
 	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
 		http://creativecommons.org/licenses/by/4.0/
 	*/
-
 spl_autoload_register(function ($class) {
 	if ( $lib = realpath( __DIR__ . '/' . str_replace('\\', '/', $class) . '.php')) {
 		include_once $lib;
@@ -19,11 +18,12 @@ spl_autoload_register(function ($class) {
 
 });
 
+
 /*
 * upstream the autoload here by including some local file
 * autoload-local-path is included in .gitignore
 */
-if ( file_exists( __DIR__ . 'autoload-local-path.php')) {
-	include __DIR__ . 'autoload-local-path.php';
+if ( file_exists( __DIR__ . DIRECTORY_SEPARATOR . 'autoload-local-path.php')) {
+	include __DIR__ . DIRECTORY_SEPARATOR . 'autoload-local-path.php';
 
 }
