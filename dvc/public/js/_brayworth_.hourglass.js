b77c3bedebd841184e49242b8691b8347267be43
Dave Bray <david@brayworth.com.au>
2018-08-09 07:49:14 +1000

    time for a change

--- dvc/public/js/_brayworth_.hourglass.js
+++ dvc/public/js/_brayworth_.hourglass.js
@@ -12,7 +12,7 @@ var hourglass = _brayworth_.hourglass = {
 	on  : function( msg) {
 		if ( this.h) this.off();
 
-		let inner = $('<i class="fa fa-spinner fa-pulse fa-4x text-white" style="position: fixed; top: 50%; left: 48%" />');
+		let inner = $('<i class="fa fa-fa-circle-o-notch fa-pulse fa-4x text-white" style="position: fixed; top: 50%; left: 48%" />');
 		if (!!msg) {
 			inner = $('<h1 class="p-4 text-white text-center" style="position: fixed; top: 50%; width: 100%" />')
 				.html( msg)
