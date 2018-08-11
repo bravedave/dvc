18f7097e04c76d13535b29d7f3ac5467067d9826
Dave Bray <david@brayworth.com.au>
2018-08-12 07:33:14 +1000

    new asset controller for serving boostrap from git/composer install

---
+++ dvc/controller/assets.php
@@ -0,0 +1,43 @@
+<?php
+/*
+	David Bray
+	BrayWorth Pty Ltd
+	e. david@brayworth.com.au
+
+	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
+		http://creativecommons.org/licenses/by/4.0/
+	*/
+
+class assets extends Controller {
+  protected function _index() {}
+
+  public function bootstrap( $type = 'css', $version = 4) {
+    if ( 4 == (int)$version) {
+      $root = realpath( sprintf( '%s/../../../../twbs', __DIR__));
+      if ( $root) {
+        $path = realpath( sprintf( '%s/bootstrap/dist', $root));
+        // printf( '%s<br />', $path);
+        if ( 'css' == $type) {
+          $lib = sprintf( '%s/css/bootstrap.min.css',$path);
+          \sys::serve( $lib);
+          // printf( '%s<br />', $lib);
+
+        }
+        elseif ( 'js' == $type) {
+          $lib = sprintf( '%s/js/bootstrap.bundle.min.js',$path);
+          \sys::serve( $lib);
+          // printf( '%s<br />', $lib);
+
+        }
+
+      }
+      else {
+        throw new \Exception( 'Cannot locate twbs bootstrap - install with compose require twbs/bootstrap');
+
+      }
+
+    }
+
+  }
+
+}
