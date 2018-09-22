9faf0e23f97d6dc97c1219aa1a989ea4cb15b2c2
Dave Bray <david@brayworth.com.au>
2018-09-23 06:37:31 +1000

    declaration

--- dvc/js/_brayworth_.hourglass.js
+++ dvc/js/_brayworth_.hourglass.js
@@ -6,7 +6,7 @@
 	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
 		http://creativecommons.org/licenses/by/4.0/
 	*/
-var hourglass = _brayworth_.hourglass = {
+let hourglass = _brayworth_.hourglass = {
 	h : false,
 
 	on  : function( msg) {
