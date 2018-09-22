e1597b568c6efacf3eb4e4bb6f365ac18f34223b
Dave Bray <david@brayworth.com.au>
2018-09-23 06:19:47 +1000

    shrink

--- dvc/js/_brayworth_.browser.js
+++ dvc/js/_brayworth_.browser.js
@@ -7,14 +7,9 @@
 		http://creativecommons.org/licenses/by/4.0/
 
 */
-
-if ( typeof _brayworth_ == 'undefined')
-	var _brayworth_ = {};
-
 _brayworth_.browser = {}
 
 _brayworth_.browser.isIPhone = navigator.userAgent.toLowerCase().indexOf('iphone') > -1;
 _brayworth_.browser.isIPad = navigator.userAgent.toLowerCase().indexOf('ipad') > -1;
 _brayworth_.browser.isChromeOniOS = _brayworth_.browser.isIPhone && navigator.userAgent.toLowerCase().indexOf('CriOS') > -1;
 _brayworth_.browser.isMobileDevice = _brayworth_.browser.isIPhone || _brayworth_.browser.isIPad;
-
