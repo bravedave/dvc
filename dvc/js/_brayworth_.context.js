c59319c015828afa9cdd86956f1181907418ba6b
Dave Bray <david@brayworth.com.au>
2018-12-28 15:20:40 +1000

    Update _brayworth_.context.js

--- dvc/js/_brayworth_.context.js
+++ dvc/js/_brayworth_.context.js
@@ -70,8 +70,8 @@ _brayworth_.context = function() {
 
 			}
 
-			if ( !!e.pageY) { css.top = Math.max( e.pageY + 5, 0); }
-			if ( !!e.pageX) { css.left = Math.max( e.pageX + 5, 0); }
+			if ( !!e.pageY) { css.top = Math.max( e.pageY + 2, 0); }
+			if ( !!e.pageX) { css.left = Math.max( e.pageX + 2, 0); }
 
 			//~ console.log( this.root.width());
 
