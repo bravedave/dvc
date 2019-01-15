ec4784e936ca43fedfdea3bd6deaa95e30cd49bd
Dave Bray <david@brayworth.com.au>
2019-01-15 11:24:39 +1000

    Update _brayworth_.growl.js

--- dvc/js/_brayworth_.growl.js
+++ dvc/js/_brayworth_.growl.js
@@ -253,11 +253,6 @@
 $(document).ready( function() {
 	if ( 4 == _brayworth_.bootstrap_version()) {
 		_brayworth_.growl = function( p) { return _brayworth_.toaster( p); }
-		console.log( 'new growler', _brayworth_.bootstrap_version());
-
-	}
-	else {
-		console.log( 'legacy growler', _brayworth_.bootstrap_version());
 
 	}
 
