73692b5935a0035ee1c8433653643d858496a714
Dave Bray <david@brayworth.com.au>
2019-04-03 22:54:30 +1000

    Update _brayworth_.fileDragDropHandler.js

--- dvc/js/_brayworth_.fileDragDropHandler.js
+++ dvc/js/_brayworth_.fileDragDropHandler.js
@@ -20,136 +20,159 @@
 			});
 
 	*/
-_brayworth_.fileDragDropContainer = function() {
-	//~ let c = $('<div />');
+(function( _b_ ) {
+	_b_.fileDragDropContainer = function() {
+		//~ let c = $('<div />');
 
-	//~ let _c = $('<div class="box__uploading" />').appendTo( c);
+		//~ let _c = $('<div class="box__uploading" />').appendTo( c);
 
-	//~ let __c = $('<div class="box__fill text-center text-truncate">uploading</div>').appendTo( _c);
+		//~ let __c = $('<div class="box__fill text-center text-truncate">uploading</div>').appendTo( _c);
 
-	//~ $('<i class="fa fa-spinner fa-pulse fa-2x fa-fw" />').appendTo( __c);
+		//~ $('<i class="fa fa-spinner fa-pulse fa-2x fa-fw" />').appendTo( __c);
 
-	//~ return ( c);
+		//~ return ( c);
 
-	let c = $('<div class="progress box__uploading" />');
+		let c = $('<div class="progress box__uploading" />');
 
-	$('<div class="progress-bar progress-bar-striped box__fill" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" />').appendTo( c);
+		$('<div class="progress-bar progress-bar-striped box__fill" role="progressbar" style="width: 0%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" />').appendTo( c);
 
-	return ( c);
+		return ( c);
 
 
-}
-;
-
-_brayworth_.fileDragDropHandler = function( params) {
-	let _el = $(this);
-
-	let options = {
-		url : false,
-		postData : {},
-		onUpload : function( response) {},
 	}
+	;
+	let uploader = function( params) {
+		let options = $.extend({
+			url : false,
+			postData : {},
+			onUpload : function( response) {},
+			droppedFiles : {},
+			host : false,
 
-	$.extend( options, params);
+		}, params);
 
-	if ( !options.url)
-		throw 'Invalid upload url';
+		let data = new FormData();
+		for(let o in options.postData) {
+			data.append( o, options.postData[o]);
 
-	let isAdvancedUpload = (function() {
-		let div = document.createElement('div');
-		return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-	})();
+		}
 
-	if ( isAdvancedUpload && !_el.hasClass('has-advanced-upload')) {
-		//~ console.log( 'setup has-advanced-upload');
-		_el
-		.addClass('has-advanced-upload')
-		.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
-			e.preventDefault(); e.stopPropagation();
-		})
-		.on('dragover dragenter', function() {
-			$(this).addClass('is-dragover');
-		})
-		.on('dragleave dragend drop', function() {
-			$(this).removeClass('is-dragover');
-		})
-		.on('drop', function(e) {
-			e.preventDefault();
-			let droppedFiles = e.originalEvent.dataTransfer.files;
-			//~ console.log( droppedFiles);
-
-			if (droppedFiles) {
-				//~ console.log( 'droppedFiles');
-
-				let data = new FormData();
-				for(let o in options.postData) {
-					data.append( o, options.postData[o]);
-
-				}
-
-				$.each( droppedFiles, function(i, file) {
-					data.append('files-'+i, file);
-
-				});
-
-				$('.box__fill', _el).css('width','0');
-				_el.addClass('is-uploading');
-
-				$.ajax({
-					url: options.url,
-					type: 'POST',
-					data: data,
-					dataType: 'json',
-					cache: false,
-					contentType: false,
-					processData: false,
-					xhr: function() {
-						let xhr = new window.XMLHttpRequest();
-						xhr.upload.addEventListener("progress", function (e) {
-							if (e.lengthComputable)
-								$('.box__fill', _el).css('width', ( e.loaded / e.total * 100) + '%');
-
-						})
-
-						return xhr;
-
-					}
-
-				})
-				.done( function( response) {
-					if ( 'ack' == response.response) {
-						$.each( response.data, function( i, j) {
-							$('body').growl( j);
-
-						})
-
-					}
-					else {
-						$('body').growl( response);
-
-					}
-
-					options.onUpload( response);
-
-				})
-				.always( function( r) {
-					_el.removeClass('is-uploading');
-
-				})
-				.fail( function( r) {
-					console.warn(r);
-					_brayworth_.modal({
-						title : 'Upload Error',
-						text : 'there was an error uploading the attachments<br />we recommend you reload your browser'
-					});
-
-				});
-
-			}
+		$.each( droppedFiles, function(i, file) {
+			data.append('files-'+i, file);
 
 		});
 
-	}	// if (isAdvancedUpload && !_el.hasClass('has-advanced-upload'))
+		let progressBar = $('.box__fill', options.host);
+		progressBar
+			.css('width', '0')
+			.attr('aria-valuenow', ( e.loaded / e.total * 100));
 
-}
-;
+		options.host.addClass('is-uploading');
+
+		$.ajax({
+			url: options.url,
+			type: 'POST',
+			data: data,
+			dataType: 'json',
+			cache: false,
+			contentType: false,
+			processData: false,
+			xhr: function() {
+				let xhr = new window.XMLHttpRequest();
+				xhr.upload.addEventListener("progress", function (e) {
+					//~ if (e.lengthComputable)
+						//~ $('.box__fill', options.host).css('width', ( e.loaded / e.total * 100) + '%');
+					if (e.lengthComputable) {
+						progressBar
+							.css('width', ( e.loaded / e.total * 100) + '%')
+							.attr('aria-valuenow', ( e.loaded / e.total * 100));
+
+					}
+
+				})
+
+				return xhr;
+
+			}
+
+		})
+		.done( function( d) {
+			if ( 'ack' == d.response) {
+				$.each( d.data, function( i, j) {
+					$('body').growl( j);
+
+				})
+
+			}
+			else {
+				$('body').growl( d);
+
+			}
+
+			options.onUpload( d);
+
+		})
+		.always( function( r) {
+			options.host.removeClass('is-uploading');
+
+		})
+		.fail( function( r) {
+			console.warn(r);
+			_b_.modal({
+				title : 'Upload Error',
+				text : 'there was an error uploading the attachments<br />we recommend you reload your browser'
+			});
+
+		});
+
+	}
+	;
+	_b_.fileDragDropHandler = function( params) {
+		let _el = $(this);
+
+		let options = $.extend( {
+			url : false,
+			host : _el
+
+		}, params);
+
+		if ( !options.url)
+			throw 'Invalid upload url';
+
+		let isAdvancedUpload = (function() {
+			let div = document.createElement('div');
+			return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+		})();
+
+		if ( isAdvancedUpload && !options.host.hasClass('has-advanced-upload')) {
+
+			//~ console.log( 'setup has-advanced-upload');
+			options.host
+			.addClass('has-advanced-upload')
+			.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+				e.preventDefault(); e.stopPropagation();
+			})
+			.on('dragover dragenter', function() {
+				$(this).addClass('is-dragover');
+			})
+			.on('dragleave dragend drop', function() {
+				$(this).removeClass('is-dragover');
+			})
+			.on('drop', function(e) {
+				e.preventDefault();
+				let droppedFiles = e.originalEvent.dataTransfer.files;
+
+				if (droppedFiles) {
+					options.droppedFiles = droppedFiles;
+					uploader( options);
+
+				}
+
+			});
+
+		}	// if (isAdvancedUpload && !options.host.hasClass('has-advanced-upload'))
+
+	}
+	;
+
+})( _brayworth_ );
