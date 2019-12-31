35cc8405dc1a4b1dccd2c580907639d2ee38205d
Dave Bray <david@brayworth.com.au>
2019-12-31 15:22:53 +1000

    changing logging

--- autoloader.php
+++ autoloader.php
@@ -1,17 +1,18 @@
 <?php
 /*
-	David Bray
-	BrayWorth Pty Ltd
-	e. david@brayworth.com.au
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
+ *      http://creativecommons.org/licenses/by/4.0/
+ *
+*/
 
-	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
-		http://creativecommons.org/licenses/by/4.0/
-	*/
-
-spl_autoload_register(function ($class) {
+spl_autoload_register( function ($class) {
 	if ( $lib = realpath( __DIR__ . '/' . str_replace('\\', '/', $class) . '.php')) {
 		include_once $lib;
-		dvc\sys::logloader( sprintf( 'lib: %s', $lib ));
+		dvc\core\load::logger( sprintf( 'lib: %s', $lib ));
 		return ( true);
 
 	}
