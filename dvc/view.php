fa5eabfaeec5a5be61c97c4f496f9662d26b11c9
Dave Bray <david@brayworth.com.au>
2020-01-08 09:02:35 +1000

    Update view.php

--- dvc/view.php
+++ dvc/view.php
@@ -12,19 +12,27 @@ namespace dvc;
 
 class view {
 	public $data = null;
-	public $rootPath = null;
 	public $loadName = '?';
 	public $title = '';
 	public $wrap = [];
-	public $debug = true;
+	public $debug = false;
+
+	protected $paths = [];
+	protected $rootPath = false;
 
 	function __construct( $data = null) {
-		if ( $app = application::app())
-			$this->rootPath = $app->getRootPath();
+		if ( $app = application::app()) {
+			$this->paths[] = $this->rootPath = $app->getRootPath();
+			foreach ( $app->getPaths() as $path) {
+				$this->paths[] = $path;
 
-		else
-			$this->rootPath = __DIR__;
+			}
 
+		}
+		else {
+			$this->paths[] = $this->rootPath = __DIR__;
+
+		}
 
 		$this->data = $data;
 
@@ -38,10 +46,7 @@ class view {
 	protected function _wrap() {
 		if ( count( (array)$this->wrap)) {
 			foreach( (array)$this->wrap as $wrap) {
-				if ( $wrap) {
-					printf( '<div class="%s">', $wrap);
-
-				}
+				if ( $wrap) printf( '<div class="%s">', $wrap);
 
 			}
 
@@ -54,8 +59,7 @@ class view {
 	protected function _unwrap() {
 		if ( count( (array)$this->wrap)) {
 			foreach( (array)$this->wrap as $wrap) {
-				if ( $wrap)
-					printf( '</div><!-- wrap:div class="%s" -->', $wrap);
+				if ( $wrap) printf( '</div><!-- wrap:div class="%s" -->', $wrap);
 
 			}
 
@@ -88,31 +92,40 @@ class view {
 	}
 
 	function load( $name) {
-		if ( substr($name, 0, strlen($this->rootPath)) === $this->rootPath) {
-			if ( file_exists( $name)) {
-				$parts = pathinfo( $name);
-				$this->loadName = $parts['filename'];
-				$path = $name;
-				//~ \sys::logger( sprintf( 'dvc\view->load :: rootpath in name %s', $this->loadName));
+		$inPath = false;
+		foreach( $this->paths as $path) {
+			if ( $path) {
+				if ( substr( $name, 0, strlen( $path)) === $path) {
+					if ( file_exists( $name)) {
+						$parts = pathinfo( $name);
+						$this->loadName = $parts['filename'];
+						$path = $name;
+						$inPath = true;
+						if ( $this->debug) \sys::logger( sprintf('<%s> : found in path : %s', $name, __METHOD__));
 
-			}
-			elseif ( file_exists( $name . '.php')) {
-				$name .= '.php';
+						break;
 
-				$parts = pathinfo( $name);
-				$this->loadName = $parts['filename'];
-				$path = $name;
-				//~ \sys::logger( sprintf( 'dvc\view->load :: rootpath in name %s', $this->loadName));
+					}
+					elseif ( file_exists( $name . '.php')) {
+						$name .= '.php';
+						$parts = pathinfo( $name);
+						$this->loadName = $parts['filename'];
+						$path = $name;
+						$inPath = true;
+						if ( $this->debug) \sys::logger( sprintf('<%s> : found in path : %s', $name, __METHOD__));
 
-			}
-			else {
-				\sys::logger( sprintf( 'dvc\view->load :: rootpath in name - not found %s', $name));
-				throw new Exceptions\ViewNotFound;
+						break;
+
+					}
+
+				}
 
 			}
 
 		}
-		else {
+
+		if ( !$inPath) {
+			if ( $this->debug) \sys::logger( sprintf('<%s> : NOT in path (%s) : %s', $name, implode( ', ', $this->paths), __METHOD__));
 			$this->loadName = $name;
 			$path = sprintf( '%s/app/views/%s.php', $this->rootPath, $name );
 
@@ -120,15 +133,15 @@ class view {
 
 		if ( file_exists( $path)) {
 			$this
-				->_wrap()
-				->_load( $path)
-				->_unwrap();
+			->_wrap()
+			->_load( $path)
+			->_unwrap();
 
 			return ( true);
 
 		}
 		else {
-			//~ sys::logger( sprintf( 'not found::%s', $path ));
+			if ( $this->debug) \sys::logger( sprintf('<%s> : NOT found : %s', $name, __METHOD__));
 
 			$path = sprintf( '%s/views/%s.php', __DIR__, $name );
 			if ( file_exists( $path)) {
@@ -156,7 +169,7 @@ class view {
 				}
 
 				printf( 'view::%s - not found<br />', $name );
-				printf( 'root::%s<br />', $this->rootPath );
+				printf( 'root::%s<br />', $this->rootPath);
 				print '<br />';
 
 			}
