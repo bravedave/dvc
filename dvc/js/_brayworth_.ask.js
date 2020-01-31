ab7d675e4f3c9bd9e825834828eab17d0394ae1b
Dave Bray <david@brayworth.com.au>
2020-01-31 22:15:15 +1000

    Update _brayworth_.ask.js

--- dvc/js/_brayworth_.ask.js
+++ dvc/js/_brayworth_.ask.js
@@ -22,26 +22,27 @@
  * */
 
 _brayworth_.ask = ( params) => {
-    let dlg = $('<div class="modal" tabindex="-1" role="dialog"><div class="modal-dialog modal-dialog-centered modal-sm" role="document" ><div class="modal-content"><div class="modal-header text-white bg-dark"><h5 class="modal-title text-truncate" title="Modal">Modal</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" /></div></div ></div >');
+    let dlg = $('<div class="modal" tabindex="-1" role="dialog"><div class="modal-dialog modal-dialog-centered modal-sm" role="document" ><div class="modal-content"><div class="modal-header py-2"><h5 class="modal-title text-truncate" title="Modal">Modal</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body" /></div></div ></div >');
 
     let options = $.extend({
         text: 'Question',
         title: 'Topic',
+        headClass: 'text-white bg-dark',
         beforeOpen: function () {
             let modal = this;
             $('.modal-title', modal).html(options.title);
             $('.modal-body', modal).append(options.text);
 
-            let footer = $('<div class="modal-footer" />').appendTo($('.modal-content', modal));
+            let footer = $('<div class="modal-footer py-0" />').appendTo($('.modal-content', modal));
 
             $.each( options.buttons, function (key, j) {
                 $('<button class="btn btn-light" />')
-                    .html(key)
-                    .appendTo(footer)
-                    .on('click', function (e) {
-                        j.call(modal, e);
+                .html(key)
+                .appendTo(footer)
+                .on('click', function (e) {
+                    j.call(modal, e);
 
-                    });
+                });
 
             });
 
@@ -59,7 +60,10 @@ _brayworth_.ask = ( params) => {
     });
 
     dlg.appendTo( 'body');
+    $('.modal-header', dlg).addClass(options.headClass);
+    options.beforeOpen.call( dlg);
+    dlg.modal('show');
 
-    return _brayworth_.loadModal(options);	// a promise
+    return dlg;	// a promise
 
 };
