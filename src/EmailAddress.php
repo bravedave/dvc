0e7739927bcc143d791604936e7b217541c842ad
Dave Bray <david@brayworth.com.au>
2020-03-01 16:12:21 +1000

    license update

--- src/EmailAddress.php
+++ src/EmailAddress.php
@@ -1,14 +1,13 @@
 <?php
-/**
+/*
  * David Bray
  * BrayWorth Pty Ltd
  * e. david@brayworth.com.au
  *
- * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
- * 	http://creativecommons.org/licenses/by/4.0/
+ * MIT License
  *
- * DO NOT change this file
- * Copy it to <application>/app and modify it there
- */
+ *  DO NOT change this file
+ *  Copy it to <application>/app/dvc/ and modify it there
+*/
 
 class EmailAddress extends dvc\EmailAddress {}
