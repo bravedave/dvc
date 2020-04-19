e539fe5e6e3e4d24bb5ce0f40293d6d98e062e9b
Dave Bray <david@brayworth.com.au>
2020-04-19 17:11:25 +1000

    Undo changes

--- src/dvc/pages/bootstrap4.php
+++ src/dvc/pages/bootstrap4.php
@@ -10,4 +10,16 @@
 
 namespace dvc\pages;
 
-class bootstrap4 extends bootstrap {}
+class bootstrap4 extends bootstrap {
+
+	function __construct( $title = '' ) {
+		\config::$BOOTSTRAP_VERSION = '4';
+		self::$Bootstrap_Version = '4';
+
+		parent::__construct( $title );
+
+		$this->topOfPage = [];
+
+	}
+
+}
