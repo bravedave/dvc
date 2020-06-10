6ccd2c616bb4ae47ba47d60b29447ec43d8737f1
Dave Bray <david@brayworth.com.au>
2020-06-10 15:52:29 +1000

    remove looping (doh)

--- src/dvc/controller/home.php
+++ src/dvc/controller/home.php
@@ -28,50 +28,38 @@ class home extends Controller {
 
 	protected function _index( $data = '' ) {
 		// just points into the documentation
-		if ( $data == '') {
 
-			$readme = implode( DIRECTORY_SEPARATOR, [
-				dirname( dirname( dirname( __DIR__))),
-				'Readme.md'
+		$readme = implode( DIRECTORY_SEPARATOR, [
+			dirname( dirname( dirname( __DIR__))),
+			'Readme.md'
 
-			]);
+		]);
 
-			// \sys::logger( sprintf('<%s> %s', $readme, __METHOD__));
+		// \sys::logger( sprintf('<%s> %s', $readme, __METHOD__));
 
-			$primary = [ $readme];
-			if ( file_exists( implode( DIRECTORY_SEPARATOR, [ $this->rootPath, 'controller', 'hello.php' ]))) {
-				$primary[] = 'docs/sample';
-
-			}
-
-			$this->render([
-				'title' => $this->title,
-				'primary' => $primary,
-				'secondary' => 'docs/contents'
-
-			]);
+		$primary = [ $readme];
+		$sample = implode( DIRECTORY_SEPARATOR, [ $this->rootPath, 'controller', 'hello.php' ]);
+		if ( file_exists( $sample)) {
+			$primary[] = 'docs/sample';
 
 		}
-		else {
-			parent::index();
 
-		}
+		$this->render([
+			'title' => $this->title,
+			'primary' => $primary,
+			'secondary' => 'docs/contents'
+
+		]);
 
 	}
 
 	public function index( $data = '') {
-		//~ if ( !config::checkDBconfigured())
-			//~ Response::redirect( url::$URL . 'install/db' );
 
 		/*
 			if you set this you will get some stats in the system log
 			about how many loads have occurrered	*/
 		//~ sys::loaderCounter( new dvc\hitter('total loads'));
-
-		$this->isPost() ?
-			$this->postHandler() :
-			$this->_index( $data);
-
+		parent::index( $data);
 
 	}
 
