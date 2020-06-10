875a6b079be179f6eaec10649cb900aecad43fba
Dave Bray <david@brayworth.com.au>
2020-06-10 15:52:08 +1000

    Rename to allow dynamic reference to server port

--- src/dvc/views/docs/sample.md
+++
@@ -1,2 +0,0 @@
-  * There is a sample controller called hello in the example/application/controller folder
-    * http://localhost/hello
---
+++ src/dvc/views/docs/sample.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/  ?>
+
+<p>There is a sample controller called hello in the example/application/controller folder
+  <?= sprintf( '<a href="%s">%s</a>',
+    strings::url( 'hello'),
+    strings::url( 'hello')
+  )
+
+  ?></p>
