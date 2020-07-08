df2219365a379709417d202d205cff53555d2079
Dave Bray <david@brayworth.com.au>
2020-07-08 17:57:41 +1000

    license

--- src/dvc/js/_brayworth_.get.js
+++ src/dvc/js/_brayworth_.get.js
@@ -3,10 +3,9 @@
  * BrayWorth Pty Ltd
  * e. david@brayworth.com.au
  *
- * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
- *      http://creativecommons.org/licenses/by/4.0/
+ * MIT License
  *
-*/
+ * */
 /*jshint esversion: 6 */
 _brayworth_.get = function( url) {
 	return new Promise(function(resolve, reject) {
