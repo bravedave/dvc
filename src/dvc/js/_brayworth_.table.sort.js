e0b57c6e07d6567fc8afc167e76273ffbf6dd2ad
Dave Bray <david@brayworth.com.au>
2020-07-13 15:15:37 +1000

    tweak to return promise

--- src/dvc/js/_brayworth_.table.sort.js
+++ src/dvc/js/_brayworth_.table.sort.js
@@ -10,63 +10,66 @@
 ( _ => {
 	_.table = {
 		'sortOn' : ( table, key, sorttype, order) => {
-			//~ var debug = true;
-			let debug = false;
-			let tbody = $('tbody', table);
-			if ( !tbody) tbody = table;
+			return new Promise( ( resolve, reject) => {
+				//~ var debug = true;
+				let debug = false;
+				let tbody = $('tbody', table);
+				if ( !tbody) tbody = table;
 
-			if ( 'undefined' == typeof order)
-				order = ( tbody.data('order') == "desc" ? "asc" : "desc" );
-			tbody.data('order', order);
+				if ( 'undefined' == typeof order)
+					order = ( tbody.data('order') == "desc" ? "asc" : "desc" );
+				tbody.data('order', order);
 
-			if ( !sorttype)
-				sorttype = 'string';
+				if ( !sorttype)
+					sorttype = 'string';
 
-			let warn = true;
+				let warn = true;
 
-			let items = tbody.children('tr');
+				let items = tbody.children('tr');
 
-			if ( debug) console.log( key, sorttype, order, items.length );
+				if ( debug) console.log( key, sorttype, order, items.length );
 
-			items.sort(function sortItem(a, b) {
-				let ae = $(a).data(key);
-				let be = $(b).data(key);
-				if ( /undefined/.test( typeof ae ) || /undefined/.test( typeof ae )) {
-					ae = $(a).data('key-' + key);
-					be = $(b).data('key-' + key);
+				items.sort(function sortItem(a, b) {
+					let ae = $(a).data(key);
+					let be = $(b).data(key);
+					if ( /undefined/.test( typeof ae ) || /undefined/.test( typeof ae )) {
+						ae = $(a).data('key-' + key);
+						be = $(b).data('key-' + key);
 
-					if ( warn) console.warn( 'table sorting is not jQuery3 compatible');
-					warn = false;
+						if ( warn) console.warn( 'table sorting is not jQuery3 compatible');
+						warn = false;
 
-				}
+					}
 
-				if ( debug) console.log( key, ae, be, sorttype, order );
+					if ( debug) console.log( key, ae, be, sorttype, order );
 
-				if (sorttype == "numeric") {
-					if ( 'undefined' == typeof ae) ae = 0;
-					if ( 'undefined' == typeof be) be = 0;
-					return ( Number(ae) - Number(be));
+					if (sorttype == "numeric") {
+						if ( 'undefined' == typeof ae) ae = 0;
+						if ( 'undefined' == typeof be) be = 0;
+						return ( Number(ae) - Number(be));
 
-				}
-				else {
-					if ( 'undefined' == typeof ae) ae = '';
-					if ( 'undefined' == typeof be) be = '';
-					return ( String( ae).toUpperCase().localeCompare( String( be).toUpperCase()));
+					}
+					else {
+						if ( 'undefined' == typeof ae) ae = '';
+						if ( 'undefined' == typeof be) be = '';
+						return ( String( ae).toUpperCase().localeCompare( String( be).toUpperCase()));
 
-				}
+					}
+
+				});
+
+				$.each(items, function (i, e) {
+					if (order == "desc")
+						{ tbody.prepend(e); }
+					else
+						{ tbody.append(e); }
+
+				});
+
+				if (!( table instanceof jQuery)) table = $(table);
+				table.trigger('update-line-numbers');
 
 			});
-
-			$.each(items, function (i, e) {
-				if (order == "desc")
-					{ tbody.prepend(e); }
-				else
-					{ tbody.append(e); }
-
-			});
-
-			if (!( table instanceof jQuery)) table = $(table);
-			table.trigger('update-line-numbers');
 
 		},
 
@@ -83,7 +86,7 @@
 
 			let sorttype = $(this).data("sorttype");
 
-			_.table.sortOn( table, key, sorttype);	//~ console.log( key );
+			return _.table.sortOn( table, key, sorttype);	//~ console.log( key );
 
 		}
 
