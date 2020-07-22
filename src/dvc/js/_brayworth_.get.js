4d370ebd1b8aa7e7e807e1fd39519c46e0a65382
Dave Bray <david@brayworth.com.au>
2020-07-22 11:33:37 +1000

    dev

--- src/dvc/js/_brayworth_.get.js
+++ src/dvc/js/_brayworth_.get.js
@@ -7,26 +7,29 @@
  *
  * */
 /*jshint esversion: 6 */
-_brayworth_.get = function( url) {
-	return new Promise(function(resolve, reject) {
-		let req = new XMLHttpRequest();
-		req.open('GET', url);
+( _ => {
+	_.get = url => {
+		return new Promise( (resolve, reject) => {
+			let req = new XMLHttpRequest();
+			req.open('GET', url);
 
-		req.onload = function() {
-			// This is called even on 404 etc so check the status
-			if (req.status == 200) {
-				resolve( req.response);	// Resolve the promise with the response text
-			}
-			else {
-				reject( Error( req.statusText)); // Otherwise reject with the status text which will hopefully be a meaningful error
+			req.onload = function() {
+				// This is called even on 404 etc so check the status
+				if (req.status == 200) {
+					resolve( req.response);	// Resolve the promise with the response text
+				}
+				else {
+					reject( Error( req.statusText)); // Otherwise reject with the status text which will hopefully be a meaningful error
 
-			}
+				}
 
-		};
+			};
 
-		req.onerror = function() { reject( Error("Network Error")); };	// Handle network errors
-		req.send();		// Make the request
+			req.onerror = e => { reject( Error('Network Error')); };	// Handle network errors
+			req.send();		// Make the request
 
-	});
+		});
 
-};
+	};
+
+}) (_brayworth_);
