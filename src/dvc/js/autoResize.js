416091d0fccbaaab0c962b8c02484307c0b07951
Dave Bray <david@brayworth.com.au>
2020-08-05 16:46:10 +1000

    add autoResize option for textarea

---
+++ src/dvc/js/autoResize.js
@@ -0,0 +1,49 @@
+/**
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+ * */
+/*jshint esversion: 6 */
+($ => {
+    $.fn.autoResize = function () {
+        let hiddenDiv = $('<div class="autosize-common autosize-hiddendiv"></div>');
+
+        hiddenDiv
+            .width(this.width())
+            .appendTo(this.parent());
+
+
+        let update = function () {
+            let _me = (this instanceof jQuery ? this : $(this));
+            let content = _me.val();
+
+            hiddenDiv
+                .css('line-height', _me.css('line-height'))
+                .width(_me.width());
+
+            content = content.replace(/\n/g, '<br />');
+            hiddenDiv.html(content + '<br class="autosize-lbr">');
+
+            _me.css('height', (hiddenDiv.height() * 1.1) + 14);
+
+            //~ console.log('height', (hiddenDiv.height()*1.1) + 14);
+
+        }
+
+        this
+            .addClass('autosize-textarea autosize-common')
+            .off('keyup.autoResize')
+            .off('change.autoResize')
+            .on('keyup.autoResize', update)
+            .on('change.autoResize', update);
+
+        update.call(this);
+
+        return (this);
+
+    }
+
+})(jQuery);
--- src/dvc/jslib.php
+++ src/dvc/jslib.php
@@ -54,6 +54,7 @@ abstract class jslib {
 		'js/_brayworth_.Vue.js',
 		'js/_brayworth_.Vue.block.js',
 		'js/autofill.js',
+		'js/autoResize.js',
 		'js/brayworth.js',
 		'js/js.cookie.js',
 		'js/templation.js'
