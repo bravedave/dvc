bf15f166f2b7fca3df88a41572a4445212a263ba
Dave Bray <david@brayworth.com.au>
2020-11-13 14:10:25 +1000

    add GoodStreetString function

--- src/dvc/strings.php
+++ src/dvc/strings.php
@@ -393,6 +393,40 @@ abstract class strings {
 
 	}
 
+	static function GoodStreetString( $street ) {
+		if ( preg_match( '/The\s?Drive/i', $street ))
+			return ( $street);
+		if ( preg_match( '/The\s?Avenue/i', $street ))
+			return ( $street);
+
+		$find = [
+			'@\sroad$@i','@\sroad,@i',
+			'@\sstreet$@i','@\sstreet,@i','@\sstreet\s@i',
+			'@\savenue$@i','@\savenue,@i','@\save$@i',
+			'@\sparade$@i','@\spde$@i','@\sparade,@i','@\spde,@i',
+			'@\sterrace$@i','@\stce$@i','@\sterrace,@i','@\stce,@i',
+			'@\sdrive$@i','@\sdrive,@i',
+			'@\splace$@i','@\splace,@i',
+			'@\scourt$@i','@\scourt,@i',
+			'@\screscent$@i','@\screscent,@i'
+			];
+		$replace = [
+			' Rd',' Rd,',
+			' St',' St,',' St, ',
+			' Av',' Av,',' Av,',
+			' Pd',' Pd',' Pd,',' Pd,',
+			' Tc',' Tc',' Tc,',' Tc,',
+			' Dr',' Dr,',
+			' Pl',' Pl,',
+			' Ct',' Ct,',
+			' Cres',' Cres,'
+			];
+
+
+		return ( trim( preg_replace( $find, $replace, $street ), ', '));
+
+  }
+
 	static function html2text($document) : string {
 		$search = array(
 			'@<[\/\!]*?[^<>]*?>@si',			// trim blank lines from beginning and end
