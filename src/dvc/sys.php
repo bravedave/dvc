348e3fa7e7208f18b0958592cad8ac80c0202d2d
Dave Bray <david@brayworth.com.au>
2020-11-13 14:07:07 +1000

    add googstrettstring function

--- src/dvc/sys.php
+++ src/dvc/sys.php
@@ -34,9 +34,9 @@ abstract class sys {
 	}
 
 	static function dbCheck( string $file) {
-        return 'sqlite' == \config::$DB_TYPE ?
-            new sqlite\dbCheck( self::dbi(), $file ) :
-            new \dao\dbCheck( self::dbi(), $file );
+    return 'sqlite' == \config::$DB_TYPE ?
+      new sqlite\dbCheck( self::dbi(), $file ) :
+      new \dao\dbCheck( self::dbi(), $file );
 
 	}
 
@@ -107,6 +107,40 @@ abstract class sys {
 		}
 
 	}
+
+	static function GoodStreetString( $street ) {
+		if ( preg_match( '/The\s?Drive/i', $street ))
+			return ( $street);
+		if ( preg_match( '/The\s?Avenue/i', $street ))
+			return ( $street);
+
+		$find = [
+			'@\sroad$@i','@\sroad,@i',
+			'@\sstreet$@i','@\sstreet,@i','@\sstreet\s@i',
+			'@\savenue$@i','@\savenue,@i','@\save$@i',
+			'@\sparade$@i','@\spde$@i','@\sparade,@i','@\spde,@i',
+			'@\sterrace$@i','@\stce$@i','@\sterrace,@i','@\stce,@i',
+			'@\sdrive$@i','@\sdrive,@i',
+			'@\splace$@i','@\splace,@i',
+			'@\scourt$@i','@\scourt,@i',
+			'@\screscent$@i','@\screscent,@i'
+			];
+		$replace = [
+			' Rd',' Rd,',
+			' St',' St,',' St, ',
+			' Av',' Av,',' Av,',
+			' Pd',' Pd',' Pd,',' Pd,',
+			' Tc',' Tc',' Tc,',' Tc,',
+			' Dr',' Dr,',
+			' Pl',' Pl,',
+			' Ct',' Ct,',
+			' Cres',' Cres,'
+			];
+
+
+		return ( trim( preg_replace( $find, $replace, $street ), ', '));
+
+  }
 
 	static function logging( $level = null) {
 		/**
