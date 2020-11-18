f84f5c3682203e3afdc94217c716fba21e416674
Dave Bray <david@brayworth.com.au>
2020-11-18 11:44:39 +1000

    Cache flush granularity

--- src/dvc/db.php
+++ src/dvc/db.php
@@ -15,8 +15,46 @@ class db {
 
 	public $log = false;
 
+	protected function _flushCache() {
+    if ( \config::$DB_CACHE == 'APC') {
+      /**
+       * the automatic caching is controlled by:
+       *	=> \dao\_dao->getByID addes to cache
+       *  => \dao\_dao->UpdateByID flushes the cache selectively
+       *		 - and sets flushCache to FALSE - so you won't be here
+       *
+       *	if you are here it is because Update was called casually outside
+       *	of UpdateByID <=> a master flush is required
+       */
+      $cache = \dvc\cache::instance();
+      $cache->flush();
+      if ( \config::$DB_CACHE_DEBUG || \config::$DB_CACHE_DEBUG_FLUSH) {
+        foreach ( debug_backtrace() as $e ) {
+          sys::logger( sprintf( 'post flush: %s(%s)', $e['file'], $e['line'] ));
+
+        }
+
+      }
+
+    }
+
+  }
+
 	public static function dbTimeStamp() {
 		return ( date( "Y-m-d H:i:s", time()));
+
+	}
+
+	public static function mysqli_field_type($type_id) {
+		static $types;
+
+		if (!isset($types)) {
+			$types = array();
+			$constants = get_defined_constants(true);
+			foreach ($constants['mysqli'] as $c => $n) if (preg_match('/^MYSQLI_TYPE_(.*)/', $c, $m)) $types[$n] = $m[1];
+		}
+
+		return array_key_exists($type_id, $types)? $types[$type_id] : "unKnown";
 
 	}
 
@@ -41,148 +79,12 @@ class db {
 
 	}
 
-	public function getCharSet() {
-		return ( $this->mysqli->character_set_name());
-
-	}
-
-	public function getDBName() {
-		return ( $this->dbname);
-
-	}
-
-	public function Q( $query ) {
-		if ( $this->log ) sys::logSQL( $query );
-		if ( $result = $this->mysqli->query($query)) return ( $result );
-
-
-		/****************************************
-		  * You are here because there was an error **/
-		$message = sprintf( "Error : MySQLi : %s\nError : MySQLi : %s",
-			$query,
-			$this->mysqli->error );
-
-		sys::logSQL( $message );
-		foreach ( debug_backtrace() as $e )
-			sys::logger( sprintf( '%s(%s)', $e['file'], $e['line'] ));
-
-		throw new \Exception( $message );
-
-	}
-
-	public function escape( $s ) {
-		return ( $this->mysqli->real_escape_string($s));
-
-	}
-
 	public function affected_rows() {
 		return ( $this->mysqli->affected_rows);
 
 	}
 
-	public function Insert( $table, $a ) {
-		$fA = [];
-		$fV = [];
-		foreach ( $a as $k => $v ) {
-			$fA[] = $k;
-			$fV[] = $this->mysqli->real_escape_string ($v);
-
-		}
-
-		$sql = sprintf( 'INSERT INTO `%s`(`%s`) VALUES("%s")', $table, implode( "`,`", $fA ), implode( '","', $fV ));
-
-		$this->Q( $sql);
-		return ( $this->mysqli->insert_id);
-
-	}
-
-	public function Update( $table, $a, $scope, $flushCache = true ) {
-		if ( \config::$DB_CACHE == 'APC') {
-			if ( (bool)$flushCache) {
-				/*
-				 * the automatic caching is controlled by:
-				 *	=> \dao\_dao->getByID addes to cache
-				 *  => \dao\_dao->UpdateByID flushes the cache selectively
-				 *		 - and sets flushCache to FALSE - so you won't be here
-				 *
-				 *	if you are here it is because Update was called casually outside
-				 *	of UpdateByID <=> a master flush is required
-				 */
-				$cache = \dvc\cache::instance();
-				$cache->flush();
-				if ( \config::$DB_CACHE_DEBUG || \config::$DB_CACHE_DEBUG_FLUSH) {
-					foreach ( debug_backtrace() as $e )
-						sys::logger( sprintf( 'post flush: %s(%s)', $e['file'], $e['line'] ));
-
-				}
-
-			}
-
-		}
-
-		$aX = [];
-		foreach ( $a as $k => $v )
-			$aX[] = "`$k` = '" . $this->mysqli->real_escape_string($v) . "'";
-
-		$sql = sprintf( 'UPDATE `%s` SET %s %s', $table, implode( ', ', $aX ), $scope);
-		return ( $this->Q( $sql));
-
-	}
-
-	public function fieldList( $table ) {
-		$result = $this->Q( "SHOW COLUMNS FROM `$table`");
-		$ret = array();
-		while ($row = mysqli_fetch_assoc( $result ))
-			$ret[] = $row["Field"];
-
-		return ( $ret );
-
-	}
-
-	public function fetchFields( $table ) {
-		$res = $this->Q( "SELECT * FROM `$table` LIMIT 1");
-		return ( $res->fetch_fields());
-
-	}
-
-	public function field_exists( $table, $field ) {
-		$ret = FALSE;
-
-		$result = $this->Q( "SHOW COLUMNS FROM $table");
-		if ( mysqli_num_rows($result) > 0 ) {
-			while ($row = mysqli_fetch_assoc($result)) {
-				if ( $row['Field'] == $field ) {
-					$ret = TRUE;
-					break;
-
-				}
-
-			}
-
-		}
-		return ($ret);
-
-	}
-
-	public function field_type( $v ) {
-		return ( self::mysqli_field_type($v));
-
-	}
-
-	public static function mysqli_field_type($type_id) {
-		static $types;
-
-		if (!isset($types)) {
-			$types = array();
-			$constants = get_defined_constants(true);
-			foreach ($constants['mysqli'] as $c => $n) if (preg_match('/^MYSQLI_TYPE_(.*)/', $c, $m)) $types[$n] = $m[1];
-		}
-
-		return array_key_exists($type_id, $types)? $types[$type_id] : "unKnown";
-
-	}
-
-	function dump() {
+	public function dump() {
 		if ( $dbR = $this->result( sprintf( 'SHOW TABLES FROM %s', \config::$DB_NAME ))) {
 			$uID = 0;
 			while ( $row = $dbR->fetch_row()) {
@@ -218,6 +120,110 @@ class db {
 				\config::$DB_HOST);
 
 		}
+
+  }
+
+  public function escape( $s ) {
+		return ( $this->mysqli->real_escape_string($s));
+
+	}
+
+	public function fetchFields( $table ) {
+		$res = $this->Q( "SELECT * FROM `$table` LIMIT 1");
+		return ( $res->fetch_fields());
+
+	}
+
+	public function field_exists( $table, $field ) {
+		$ret = FALSE;
+
+		$result = $this->Q( "SHOW COLUMNS FROM $table");
+		if ( mysqli_num_rows($result) > 0 ) {
+			while ($row = mysqli_fetch_assoc($result)) {
+				if ( $row['Field'] == $field ) {
+					$ret = TRUE;
+					break;
+
+				}
+
+			}
+
+		}
+		return ($ret);
+
+	}
+
+	public function field_type( $v ) {
+		return ( self::mysqli_field_type($v));
+
+  }
+
+  public function fieldList( $table ) {
+		$result = $this->Q( "SHOW COLUMNS FROM `$table`");
+		$ret = array();
+		while ($row = mysqli_fetch_assoc( $result ))
+			$ret[] = $row["Field"];
+
+		return ( $ret );
+
+	}
+
+	public function getCharSet() {
+		return ( $this->mysqli->character_set_name());
+
+	}
+
+	public function getDBName() {
+		return ( $this->dbname);
+
+	}
+
+	public function Insert( $table, $a ) {
+		$fA = [];
+		$fV = [];
+		foreach ( $a as $k => $v ) {
+			$fA[] = $k;
+			$fV[] = $this->mysqli->real_escape_string ($v);
+
+		}
+
+		$sql = sprintf( 'INSERT INTO `%s`(`%s`) VALUES("%s")', $table, implode( "`,`", $fA ), implode( '","', $fV ));
+
+		$this->Q( $sql);
+		return ( $this->mysqli->insert_id);
+
+	}
+
+	public function Q( $query ) {
+		if ( $this->log ) sys::logSQL( $query );
+		if ( $result = $this->mysqli->query($query)) return ( $result );
+
+
+		/****************************************
+		  * You are here because there was an error **/
+		$message = sprintf( "Error : MySQLi : %s\nError : MySQLi : %s",
+			$query,
+			$this->mysqli->error );
+
+		sys::logSQL( $message );
+		foreach ( debug_backtrace() as $e )
+			sys::logger( sprintf( '%s(%s)', $e['file'], $e['line'] ));
+
+		throw new \Exception( $message );
+
+	}
+
+	public function Update( $table, $a, $scope, $flushCache = true ) {
+    if ( (bool)$flushCache) $this->_flushCache();
+
+		$aX = [];
+		foreach ( $a as $k => $v ) {
+			$aX[] = "`$k` = '" . $this->mysqli->real_escape_string($v) . "'";
+
+    }
+
+		$sql = sprintf( 'UPDATE `%s` SET %s %s', $table, implode( ', ', $aX ), $scope);
+		return ( $this->Q( $sql));
 
 	}
 
--- src/dvc/sqlite/db.php
+++ src/dvc/sqlite/db.php
@@ -1,13 +1,12 @@
 <?php
 /*
-	David Bray
-	BrayWorth Pty Ltd
-	e. david@brayworth.com.au
-
-	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
-		http://creativecommons.org/licenses/by/4.0/
-
-	*/
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
 
 namespace dvc\sqlite;
 
@@ -18,17 +17,6 @@ class db {
 
 	protected static $_instance = false;
 
-	public function valid() {
-		if ( !self::$_instance)
-			self::$_instance = new self;
-
-		if ( self::$_instance)
-			return ( true);
-
-		return ( false);
-
-	}
-
 	static function instance() {
 		if ( !self::$_instance)
 			self::$_instance = new self;
@@ -36,6 +24,24 @@ class db {
 		return ( self::$_instance);
 
 	}
+
+	protected function _flushCache() {
+    if ( \config::$DB_CACHE == 'APC') {
+      /**
+       * the automatic caching is controlled by:
+       *	=> \dao\_dao->getByID addes to cache
+       *  => \dao\_dao->UpdateByID flushes the cache selectively
+       *		 - and sets flushCache to FALSE - so you won't be here
+       *
+       *	if you are here it is because Update was called casually outside
+       *	of UpdateByID <=> a master flush is required
+       */
+      $cache = \dvc\cache::instance();
+      $cache->flush();
+
+    }
+
+  }
 
 	protected function __construct() {
 		$this->_path = sprintf('%s%ssqlite.db', \config::dataPath(), DIRECTORY_SEPARATOR );
@@ -68,8 +74,152 @@ class db {
 
 	}
 
+	public function dump() {
+		if ( $tables = $this->tables()) {
+
+			$uID = 0;
+			foreach ( $tables as $table) {
+				printf( '<span data-role="visibility-toggle" data-target="bqt%s">Table: %s</span><br />%s',
+					$uID,
+					$table,
+					PHP_EOL	);
+				printf( '<blockquote id=\'bqt%s\' style="font-family: monospace; display: none;">%s',
+					$uID++,
+					PHP_EOL	);
+
+				/* Get field information for all columns */
+				if ( $fields = $this->fieldList( $table)) {
+					//~ sys::dump( $fields);
+				// 	$finfo = $res->fetch_fields();
+
+				 	foreach ($fields as $field)
+				 		printf( '<br />%s %s %s', $field->name, $field->type, ( $field->pk ? 'primary key' : ''));
+
+				}
+
+				print "</blockquote>\n";
+
+			}
+
+		}
+
+	}
+
+	public function escape( $value ) {
+		return $this->_db->escapeString( $value );
+
+	}
+
+	public function fieldList( $table ) {
+		$ret = [];
+		if ( $result = $this->result( sprintf( 'PRAGMA table_info(%s)', $table))) {
+			while ( $dto = $result->dto())
+				$ret[] = $dto;
+
+		}
+
+		return ( $ret );
+
+	}
+
 	public function getPath() {
 		return ( $this->_path);
+
+	}
+
+	public function Insert( $table, $a ) {
+		/**
+		 * Insert values into SQLite table
+		 *
+		 * Note: SQLite values must delimit with ' (single quote)
+		 *
+		 * Parameters: 	Table to update
+		 * 				array of key => values
+		 *
+		 */
+		$fA = [];
+		$fV = [];
+		foreach ( $a as $k => $v ) {
+			$fA[] = $k;
+			$fV[] = $this->escape($v);
+
+		}
+
+		$sql = sprintf( "INSERT INTO `%s`(`%s`) VALUES('%s')", $table, implode( "`,`", $fA ), implode( "','", $fV ));
+
+		$this->_db->exec( $sql);
+		return ( $this->_db->lastInsertRowID());
+
+	}
+
+	public function Q( string $sql) {
+		if ( $this->log) \sys::logSQL( $sql);
+		if ( $result = $this->_db->query( $sql)) return ( $result);
+
+		/****************************************
+		  * You are here because there was an error **/
+		$message = sprintf( "Error : SQLite : %s\nError : SQLite : %s",
+			$query,
+			$this->_db->lastErrorMsg );
+
+		\sys::logSQL( $sql);
+		foreach ( debug_backtrace() as $e )
+			\sys::logger( sprintf( '%s(%s)', $e['file'], $e['line'] ));
+
+		throw new \Exception( $message );
+
+	}
+
+	public function result(  $query) {
+		$dbResult = new dbResult( $this->Q( $query), $this);
+		return ( $dbResult);
+
+	}
+
+	public function tables() {
+		$ret = [];
+		if ( $result = $this->result( "SELECT name FROM sqlite_master WHERE type='table'")) {
+			while ( $dto = $result->dto()) {
+				if ( !preg_match( '/^sqlite_/', $dto->name))
+					$ret[] = $dto->name;
+
+			}
+
+		}
+
+		return ( $ret );
+
+	}
+
+	public function Update( $table, $a, $scope, $flushCache = TRUE ) {
+    if ( (bool)$flushCache) $this->_flushCache();
+
+		/**
+		 * Update values into SQLite table
+		 *
+		 * Note: SQLite values must delimit with ' (single quote)
+		 *
+		 * Parameters: 	Table to update
+		 * 				array of key => values
+		 * 				scope of update : e.g. 'WHERE id = 1'
+		 */
+		$aX = [];
+		foreach ( $a as $k => $v )
+			$aX[] = "`$k` = '" . $this->escape($v) . "'";
+
+		$sql = sprintf( 'UPDATE `%s` SET %s %s', $table, implode( ', ', $aX ), $scope);
+		return ( $this->Q($sql));
+
+	}
+
+	public function valid() {
+		if ( !self::$_instance)
+			self::$_instance = new self;
+
+		if ( self::$_instance)
+			return ( true);
+
+		return ( false);
 
 	}
 
@@ -108,155 +258,6 @@ class db {
 			// $this->_db = new \SQLite3( $this->_path);	// throws exception on failure
 
 		}
-
-	}
-
-	public function escape( $value ) {
-		return $this->_db->escapeString( $value );
-
-	}
-
-	public function Insert( $table, $a ) {
-		/**
-		 * Insert values into SQLite table
-		 *
-		 * Note: SQLite values must delimit with ' (single quote)
-		 *
-		 * Parameters: 	Table to update
-		 * 				array of key => values
-		 *
-		 */
-		$fA = [];
-		$fV = [];
-		foreach ( $a as $k => $v ) {
-			$fA[] = $k;
-			$fV[] = $this->escape($v);
-
-		}
-
-		$sql = sprintf( "INSERT INTO `%s`(`%s`) VALUES('%s')", $table, implode( "`,`", $fA ), implode( "','", $fV ));
-
-		$this->_db->exec( $sql);
-		return ( $this->_db->lastInsertRowID());
-
-	}
-
-	public function Update( $table, $a, $scope, $flushCache = TRUE ) {
-		if ( \config::$DB_CACHE == 'APC') {
-			if ( (bool)$flushCache) {
-				/*
-				 * the automatic caching is controlled by:
-				 *	=> \dao\_dao->getByID addes to cache
-				 *  => \dao\_dao->UpdateByID flushes the cache selectively
-				 *		 - and sets flushCache to FALSE - so you won't be here
-				 *
-				 *	if you are here it is because Update was called casually outside
-				 *	of UpdateByID <=> a master flush is required
-				 */
-				$cache = \dvc\cache::instance();
-				$cache->flush();
-
-			}
-
-		}
-
-		/**
-		 * Update values into SQLite table
-		 *
-		 * Note: SQLite values must delimit with ' (single quote)
-		 *
-		 * Parameters: 	Table to update
-		 * 				array of key => values
-		 * 				scope of update : e.g. 'WHERE id = 1'
-		 */
-		$aX = [];
-		foreach ( $a as $k => $v )
-			$aX[] = "`$k` = '" . $this->escape($v) . "'";
-
-		$sql = sprintf( 'UPDATE `%s` SET %s %s', $table, implode( ', ', $aX ), $scope);
-		return ( $this->Q($sql));
-
-	}
-
-	public function Q( string $sql) {
-		if ( $this->log) \sys::logSQL( $sql);
-		if ( $result = $this->_db->query( $sql)) return ( $result);
-
-		/****************************************
-		  * You are here because there was an error **/
-		$message = sprintf( "Error : SQLite : %s\nError : SQLite : %s",
-			$query,
-			$this->_db->lastErrorMsg );
-
-		\sys::logSQL( $sql);
-		foreach ( debug_backtrace() as $e )
-			\sys::logger( sprintf( '%s(%s)', $e['file'], $e['line'] ));
-
-		throw new \Exception( $message );
-
-	}
-
-	public function result(  $query) {
-		$dbResult = new dbResult( $this->Q( $query), $this);
-		return ( $dbResult);
-
-	}
-
-	public function dump() {
-		if ( $tables = $this->tables()) {
-
-			$uID = 0;
-			foreach ( $tables as $table) {
-				printf( '<span data-role="visibility-toggle" data-target="bqt%s">Table: %s</span><br />%s',
-					$uID,
-					$table,
-					PHP_EOL	);
-				printf( '<blockquote id=\'bqt%s\' style="font-family: monospace; display: none;">%s',
-					$uID++,
-					PHP_EOL	);
-
-				/* Get field information for all columns */
-				if ( $fields = $this->fieldList( $table)) {
-					//~ sys::dump( $fields);
-				// 	$finfo = $res->fetch_fields();
-
-				 	foreach ($fields as $field)
-				 		printf( '<br />%s %s %s', $field->name, $field->type, ( $field->pk ? 'primary key' : ''));
-
-				}
-
-				print "</blockquote>\n";
-
-			}
-
-		}
-
-	}
-
-	public function tables() {
-		$ret = [];
-		if ( $result = $this->result( "SELECT name FROM sqlite_master WHERE type='table'")) {
-			while ( $dto = $result->dto()) {
-				if ( !preg_match( '/^sqlite_/', $dto->name))
-					$ret[] = $dto->name;
-
-			}
-
-		}
-
-		return ( $ret );
-
-	}
-
-	public function fieldList( $table ) {
-		$ret = [];
-		if ( $result = $this->result( sprintf( 'PRAGMA table_info(%s)', $table))) {
-			while ( $dto = $result->dto())
-				$ret[] = $dto;
-
-		}
-
-		return ( $ret );
 
 	}
 
