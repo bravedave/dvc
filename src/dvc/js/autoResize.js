095596019ac032e72996026003cde078026fc28d
Dave Bray <david@brayworth.com.au>
2020-12-04 14:52:06 +1000

    Update autoResize.js

--- src/dvc/js/autoResize.js
+++ src/dvc/js/autoResize.js
@@ -28,8 +28,7 @@
       hiddenDiv.html(content + '<br class="autosize-lbr">');
 
       _me.css('height', (hiddenDiv.height() * 1.1) + 14);
-
-      //~ console.log('height', (hiddenDiv.height()*1.1) + 14);
+      console.log('height', (hiddenDiv.height()*1.1) + 14);
 
     }
 
