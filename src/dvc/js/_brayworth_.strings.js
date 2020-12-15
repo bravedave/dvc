069a186af82dc9969ff978f7ab64cf87304a14ca
Dave Bray <david@brayworth.com.au>
2020-12-15 12:34:22 +1000

    add toHtml prototype

--- src/dvc/js/_brayworth_.strings.js
+++ src/dvc/js/_brayworth_.strings.js
@@ -10,71 +10,9 @@
  *	'0418745334'.IsMobilePhone();
 */
 /*jshint esversion: 6 */
-(function() {	// strings
+(() => {	// strings
 	String.prototype.ltrim = function() {return this.replace(/^\s+/,"");};
-	String.prototype.rtrim = function() {return this.replace(/\s+$/,"");};
-	String.prototype.pad = function(len, padChar){
-		console.warn( 'deprecated - use string.padStart()');
-
-		if ( padChar == undefined ) {padChar = " ";}
-		if ( isNaN(len) ) { len = this.length; }
-
-		let res = this;
-		while ( res.length < len ) {res = res.concat(padChar);}
-		return ( res );
-
-	};
-
-	String.prototype.padLeft = function(len, padChar){
-		console.warn( 'debrecated - use string.padEnd()');
-
-		if ( padChar == undefined ) {padChar = " ";}
-		if ( isNaN(len) ) { len = this.length; }
-		let res = this;
-		if (res.length > len) {
-			let iStart = ( res.length - len );
-			res = res.substring( iStart );
-
-		}
-		else {
-			while ( res.length < len ) {
-				res = padChar.concat(res);
-
-			}
-
-		}
-
-		return ( res );
-
-	};
-
-	String.prototype.format = function () {
-		let args = arguments;
-		return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
-			if (m == "{{") { return "{"; }
-			if (m == "}}") { return "}"; }
-			return args[n];
-		});
-	};
-
-	String.prototype.toCapitalCase = function() {
-		let re = /\s/;
-		let words = this.split(re);
-		re = /(\S)(\S+)/;
-		let reI = /^[a-zA-Z]'[a-zA-Z]+$/;
-		for (let i = words.length - 1; i >= 0; i--) {
-			if ( words[i] != "&" ) {
-				if ( words[i].length > 3 && reI.test(words[i])) {
-					//~ alert( 'It\'s Irish' );
-					parts = words[i].split(/'/);
-					words[i] = parts[0].toUpperCase() + "'" + parts[1].substring(0,1).toUpperCase() + parts[1].substring(1).toLowerCase();
-				} else if ( re.test(words[i])) {
-					words[i] = RegExp.$1.toUpperCase() + RegExp.$2.toLowerCase();
-				}
-			}
-		}
-		return words.join(' ');
-	};
+  String.prototype.rtrim = function() {return this.replace(/\s+$/,"");};
 
 	String.prototype.AsLocalPhone = function() {
 		let p = this;
@@ -109,44 +47,6 @@
 		}
 
 		return( p );
-
-	};
-
-	String.prototype.IsPhone = function() {
-		let p = this;
-		let ns = this.replace(/[^0-9\+]/g,'');
-		if ( ns.length == 8 || ns.length == 10 || ( ns.substring(0,1) == '+' && ns.length == 12))
-			return ( true);
-
-		return( false);
-
-	};
-
-	String.prototype.IsMobilePhone = function() {
-		let p = this;
-		let ns = this.replace(/[^0-9\+]/g,'');
-
-		if ( ns.length == 10) {
-			return ( true);
-		}
-		else if ( ns.substring(0,1) == '+' && ns.length == 12) {
-			return ( true);
-
-		}
-		else if ( ns.substring(0,2) == '61' && ns.length == 11) {	/* australian mobile */
-			return ( true);
-
-		}
-		else if ( ns.substring(0,1) == '+' && ns.length == 13) {	/* italian mobile */
-			return ( true);
-
-		}
-		else if ( ns.substring(0,2) == '39' && ns.length == 12) {	/* italian mobile */
-			return ( true);
-
-		}
-
-		return( false);
 
 	};
 
@@ -191,6 +91,15 @@
 
 	};
 
+  String.prototype.format = function () {
+    let args = arguments;
+    return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
+      if (m == "{{") { return "{"; }
+      if (m == "}}") { return "}"; }
+      return args[n];
+    });
+  };
+
 	String.prototype.isEmail = function() {
 		if ( this.length < 3)
 			return ( false);
@@ -215,7 +124,112 @@
 
 		return rex.test( e.toLowerCase());
 
-	};
+  };
+
+  String.prototype.IsMobilePhone = function () {
+    let p = this;
+    let ns = this.replace(/[^0-9\+]/g, '');
+
+    if (ns.length == 10) {
+      return (true);
+    }
+    else if (ns.substring(0, 1) == '+' && ns.length == 12) {
+      return (true);
+
+    }
+    else if (ns.substring(0, 2) == '61' && ns.length == 11) {	/* australian mobile */
+      return (true);
+
+    }
+    else if (ns.substring(0, 1) == '+' && ns.length == 13) {	/* italian mobile */
+      return (true);
+
+    }
+    else if (ns.substring(0, 2) == '39' && ns.length == 12) {	/* italian mobile */
+      return (true);
+
+    }
+
+    return (false);
+
+  };
+
+  String.prototype.IsPhone = function () {
+    let p = this;
+    let ns = this.replace(/[^0-9\+]/g, '');
+    if (ns.length == 8 || ns.length == 10 || (ns.substring(0, 1) == '+' && ns.length == 12))
+      return (true);
+
+    return (false);
+
+  };
+
+  String.prototype.pad = function (len, padChar) {
+    console.warn('deprecated - use string.padStart()');
+
+    if (padChar == undefined) { padChar = " "; }
+    if (isNaN(len)) { len = this.length; }
+
+    let res = this;
+    while (res.length < len) { res = res.concat(padChar); }
+    return (res);
+
+  };
+
+  String.prototype.padLeft = function (len, padChar) {
+    console.warn('debrecated - use string.padEnd()');
+
+    if (padChar == undefined) { padChar = " "; }
+    if (isNaN(len)) { len = this.length; }
+    let res = this;
+    if (res.length > len) {
+      let iStart = (res.length - len);
+      res = res.substring(iStart);
+
+    }
+    else {
+      while (res.length < len) {
+        res = padChar.concat(res);
+
+      }
+
+    }
+
+    return (res);
+
+  };
+
+  String.prototype.toCapitalCase = function () {
+    let re = /\s/;
+    let words = this.split(re);
+    re = /(\S)(\S+)/;
+    let reI = /^[a-zA-Z]'[a-zA-Z]+$/;
+    for (let i = words.length - 1; i >= 0; i--) {
+      if (words[i] != "&") {
+        if (words[i].length > 3 && reI.test(words[i])) {
+          //~ alert( 'It\'s Irish' );
+          parts = words[i].split(/'/);
+          words[i] = parts[0].toUpperCase() + "'" + parts[1].substring(0, 1).toUpperCase() + parts[1].substring(1).toLowerCase();
+        } else if (re.test(words[i])) {
+          words[i] = RegExp.$1.toUpperCase() + RegExp.$2.toLowerCase();
+        }
+      }
+    }
+    return words.join(' ');
+  };
+
+  String.prototype.toHtml = function () {
+    let s = this;
+    s = s.replace(/\n/g, '<br>');
+    s = s.replace(/â&#8364;¢/g, '&bull;');
+    s = s.replace(/&(r|l)dquo;/g, "&quot;");
+    s = s.replace(/&(r|l)squo;/g, "&#39;");
+    //~ text = text.replace( /&ndash;/g, "-");
+    //~ text = text.replace( /&bull;/g, "*");
+
+    return (s);
+
+  };
 
 	Number.prototype.formatComma = function(x) {
 		return this.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
