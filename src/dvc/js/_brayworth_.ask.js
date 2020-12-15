b1f238692ae65e7310e5c7c4140f8568f8b0bcf9
Dave Bray <david@brayworth.com.au>
2020-12-15 10:11:12 +1000

    refactor

--- src/dvc/js/_brayworth_.ask.js
+++ src/dvc/js/_brayworth_.ask.js
@@ -20,70 +20,61 @@
 
  * */
 /*jshint esversion: 6 */
-( _ => {
-    _.ask = ( params) => {
-        let dlg = $([
-            '<div class="modal" tabindex="-1" role="dialog">',
-                '<div class="modal-dialog modal-dialog-centered modal-sm" role="document">',
-                    '<div class="modal-content">',
-                        '<div class="modal-header py-2">',
-                            '<h5 class="modal-title text-truncate" title="Modal">Modal</h5>',
-                            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>',
-                        '</div>',
-                        '<div class="modal-body"></div>',
-                    '</div>',
-                '</div>',
-            '</div>'
-            ].join('')
+( _ => _.ask = ( params) => {
+  let dlg = $([
+    '<div class="modal" tabindex="-1" role="dialog">',
+      '<div class="modal-dialog modal-dialog-centered modal-sm" role="document">',
+        '<div class="modal-content">',
+          '<div class="modal-header py-2">',
+            '<h5 class="modal-title text-truncate" title="Modal">Modal</h5>',
+            '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>',
+          '</div>',
+          '<div class="modal-body"></div>',
+          '<div class="modal-footer py-0"></div>',
+        '</div>',
+      '</div>',
+    '</div>'
+    ].join('')
 
-        );
+  );
 
-        let options = _.extend({
-            beforeOpen: function () {
-                let modal = this;
-                $('.modal-title', modal).html( options.title);
-                $('.modal-body', modal).html( options.text);
+  let options = _.extend({
+    beforeOpen: function() {
+      let modal = this;
+      $('.modal-title', modal).html( options.title);
+      $('.modal-body', modal).html( options.text);
 
-                let footer = $('<div class="modal-footer py-0"></div>').appendTo($('.modal-content', modal));
+      let footer = $('.modal-footer', modal);
+      $.each( options.buttons, ( key, j) => {
+          $('<button class="btn btn-light" type="button" />')
+          .html(key)
+          .appendTo(footer)
+          .on('click', e => j.call(modal, e));
 
-                $.each( options.buttons, ( key, j) => {
-                    $('<button class="btn btn-light" type="button" />')
-                    .html(key)
-                    .appendTo(footer)
-                    .on('click', function (e) {
-                        j.call(modal, e);
+      });
 
-                    });
+    },
+    buttons: {},
+    headClass: 'text-white bg-dark',
+    onClose : e => {},
+    removeOnClose: true,
+    text: 'Question',
+    title: 'Topic',
 
-                });
+  }, params);
 
-            },
-            buttons: {},
-            headClass: 'text-white bg-dark',
-            onClose : (e) => {},
-            removeOnClose: true,
-            text: 'Question',
-            title: 'Topic',
+  // console.log( options);
 
-        }, params);
+  if ( options.removeOnClose) {
+    dlg.on('hidden.bs.modal', function(e) { $(this).remove(); });
 
-        // console.log( options);
+  }
 
-        if ( options.removeOnClose) {
-            dlg.on('hidden.bs.modal', function(e) {
-                $(this).remove();
+  $('.modal-header', dlg).addClass(options.headClass);
+  dlg.appendTo( 'body');
+  options.beforeOpen.call( dlg);
+  dlg.modal('show');
 
-            });
-
-        }
-
-        $('.modal-header', dlg).addClass(options.headClass);
-        dlg.appendTo( 'body');
-        options.beforeOpen.call( dlg);
-        dlg.modal('show');
-
-        return dlg;	// a promise
-
-    };
+  return dlg;	// a jQuery element
 
 })(_brayworth_);
