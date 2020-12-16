e7f0a0a41b6643e8f0e28a9621b3a571b6e4d2e3
Dave Bray <david@brayworth.com.au>
2020-12-16 15:22:23 +1000

    refactor

--- src/dvc/js/_brayworth_.get.DataUri.js
+++ src/dvc/js/_brayworth_.get.DataUri.js
@@ -3,31 +3,33 @@
  * BrayWorth Pty Ltd
  * e. david@brayworth.com.au
  *
- * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
- *      http://creativecommons.org/licenses/by/4.0/
+ * MIT License
  *
-*/
+ * */
 /*jshint esversion: 6 */
-_brayworth_.get.DataUri = function( url) {
-	return new Promise( function( resolve, reject) {
-		//~ console.log( '_cms_.get.DataUri : converting : ', url);
-		let img = new Image();
+( _ => {
+  _.get.DataUri = url => {
+    return new Promise( resolve => {
+      //~ console.log( '_cms_.get.DataUri : converting : ', url);
+      let img = new Image();
+      img.onload = function () {
+        let canvas = document.createElement('canvas');
+        canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+        canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
 
-		img.onload = function () {
-			let canvas = document.createElement('canvas');
-			canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
-			canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+        canvas.getContext('2d').drawImage(this, 0, 0);
 
-			canvas.getContext('2d').drawImage(this, 0, 0);
+        // Get raw image data
+        resolve( canvas.toDataURL('image/jpeg').replace(/^data:image\/(png|jpg|jpeg);base64,/, ''));
+        //~ console.log( 'done convert');
 
-			// Get raw image data
-			resolve( canvas.toDataURL('image/jpeg').replace(/^data:image\/(png|jpg|jpeg);base64,/, ''));
-			//~ console.log( 'done convert');
+      };
 
-		};
+      img.src = url;
 
-		img.src = url;
+    });
 
-	});
+  };
 
-};
+}) (_brayworth_);
+
