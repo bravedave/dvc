b3638ca43cb924a8345acad6f4064ce0cc788e76
Dave Bray <david@brayworth.com.au>
2021-01-03 22:55:28 +1000

    tweaks

--- src/dvc/js/_brayworth_.hashScroll.js
+++ src/dvc/js/_brayworth_.hashScroll.js
@@ -3,92 +3,94 @@
  * BrayWorth Pty Ltd
  * e. david@brayworth.com.au
  *
- * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
- *      http://creativecommons.org/licenses/by/4.0/
+ * MIT License
  *
-*/
+ * */
 /*jshint esversion: 6 */
-_brayworth_.ScrollTo = function( el, params) {
+( _ => {
+  _.ScrollTo = function( el, params) {
 
-  let options = _brayworth_.extend( {
-		marginTop : 0,
-		duration : 800
+    let options = _.extend( {
+      marginTop : 0,
+      duration : 800
 
-	}, params);
+    }, params);
 
-	return ( new Promise( function( resolve, reject) {
-		let _el = ( el instanceof jQuery ? el : $(el));
-		if ( _el.length < 1) {
-			console.log( 'element not found', el);
-			resolve();
+    return ( new Promise( ( resolve, reject) => {
+      let _el = ( el instanceof jQuery ? el : $(el));
+      if ( _el.length < 1) {
+        console.log( 'element not found', el);
+        resolve();
 
-			return;
+        return;
 
-		}
+      }
 
-		let t = _el.offset().top;
-		// console.log( _el, t);
+      let t = _el.offset().top;
+      // console.log( _el, t);
 
-		let parent = _el.closest('.modal');
-		if ( parent.length == 0) {
-			let nav = $('body>nav');
-			if ( nav.length ) {
-				t -= ( nav.outerHeight());
+      let parent = _el.closest('.modal');
+      if ( parent.length == 0) {
+        let nav = $('body>nav');
+        if ( nav.length ) {
+          t -= ( nav.outerHeight());
 
-			}
-			else {
-				let hdr = $('body>header');
-				if ( hdr.length )
-					t -= ( hdr.outerHeight());
+        }
+        else {
+          let hdr = $('body>header');
+          if ( hdr.length )
+            t -= ( hdr.outerHeight());
 
-			}
+        }
 
-		}
+      }
 
-		t -= options.marginTop;
-		t = Math.max( 20, t);
+      t -= options.marginTop;
+      t = Math.max( 20, t);
 
-		if (parent.length > 0) {
-			parent.animate({ scrollTop: t}, {
-				duration : options.duration,
-				complete : resolve,
-				fail : reject,
+      if (parent.length > 0) {
+        parent.animate({ scrollTop: t}, {
+          duration : options.duration,
+          complete : resolve,
+          fail : reject,
 
-			});
+        });
 
-		}
-		else {
-			$('html,body').animate({ scrollTop: t}, {
-				duration : options.duration,
-				complete : resolve,
-				fail : reject,
+      }
+      else {
+        $('html,body').animate({ scrollTop: t}, {
+          duration : options.duration,
+          complete : resolve,
+          fail : reject,
 
-			});
+        });
 
-		}
+      }
 
-	}));
+    }));
 
-};
+  };
 
-_brayworth_.hashScroll = function() {
-	/** Scrolls the content into view **/
-	$('a[href*="#"]:not([href="#"] , .carousel-control, .ui-tabs-anchor)').on('click', function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			let target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				if ( /nav/i.test( target.prop('tagName')))
-					return;
+  _.hashScroll = () => {
+    /** Scrolls the content into view **/
+    $('a[href*="#"]:not([href="#"] , .carousel-control, .ui-tabs-anchor)').on('click', function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        let target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          if ( /nav/i.test( target.prop('tagName')))
+            return;
 
-				_brayworth_.ScrollTo( target);
+          _.ScrollTo( target);
 
-				return false;
+          return false;
 
-			}
+        }
 
-		}
+      }
 
-	});
+    });
 
-};
+  };
+
+}) (_brayworth_);
--- src/dvc/js/_brayworth_.hourglass.js
+++ src/dvc/js/_brayworth_.hourglass.js
@@ -15,7 +15,7 @@ var hourglass = _brayworth_.hourglass = {
 
 		if ( _me.h) _me.off();
 
-		return new Promise( (resolve, reject) => {
+		return new Promise( resolve => {
       let inner = $('<i class="spinner-border text-white" style="position: fixed; top: 50%; left: 48%; width: 3rem; height: 3rem;"></i>');
 			if (!!msg) {
 				inner = $('<h1 class="p-4 text-white text-center" style="position: fixed; top: 50%; width: 100%"></h1>')
