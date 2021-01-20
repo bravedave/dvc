fe9a3f788ec09b628df451abd64f6f9c20fc94ed
Dave Bray <david@brayworth.com.au>
2021-01-20 11:53:10 +1000

    spinner handler

--- src/dvc/js/spinner.js
+++ src/dvc/js/spinner.js
@@ -11,14 +11,16 @@
   $.fn.spinner = function( state) {
     if ( 'off' == String( state)) {
       let _data = this.data();
-
       this.removeClass().addClass( _data.class);
-      this.removeData( 'class');
 
     }
     else {
 
-      this.data( 'class', this.attr( 'class'));
+      if ( !this.hasData( 'class')) {
+        this.data( 'class', this.attr( 'class'));
+
+      }
+
       this.removeClass();
       this.addClass( 'grow' == String( state) ? 'spinner-grow spinner-grow-sm' :'spinner-border spinner-border-sm');
 
