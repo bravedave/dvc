5c350466b1c67018a97d75444779157a33ae67fb
Dave Bray <david@brayworth.com.au>
2021-01-24 16:00:02 +1000

    PHP8 in dev environment

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 WD=`pwd`
 PORT=$[RANDOM%1000+1024]
+php=php
+if [[ -x /usr/bin/php8 ]]; then php=php8; fi
 
 cd tests/www
 echo "this application is available at http://localhost:$PORT"
-php -S localhost:$PORT _dvc.php
+$php -S localhost:$PORT _dvc.php
 cd $WD
