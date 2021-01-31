a443795c6f120638f45ffb5e2aacc2320c162cb4
Dave Bray <david@brayworth.com.au>
2021-01-31 17:32:39 +1000

    added theme

---
+++ src/theme.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+ *  DO NOT change this file
+ *  Copy it to <application>/app/dvc/ and modify it there
+*/
+
+class theme extends dvc\theme {}
