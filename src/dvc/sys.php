d1abcc9e721b6154aae13bb97fa0fa4707117ce0
Dave Bray <david@brayworth.com.au>
2021-04-16 10:52:49 +1000

    debugging cache tweaks

--- src/dvc/sys.php
+++ src/dvc/sys.php
@@ -100,7 +100,7 @@ abstract class sys {
 
 		}
     elseif ( 'mysql' == \config::$DB_TYPE ) {
-      return \config::$DB_NAME;
+      return str_replace( '.', '_', \config::$DB_HOST . \config::$DB_NAME);
 
 		}
 		else {
