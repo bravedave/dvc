e2d96a57fc9ff715f510f0bdacdf8a11d9e268b3
Dave Bray <david@brayworth.com.au>
2021-07-11 12:22:48 +1000

    update jquery

--- src/dvc/controller/assets.php
+++ src/dvc/controller/assets.php
@@ -12,39 +12,33 @@ use dvc\cssmin;
 use dvc\jslib;
 
 class assets extends Controller {
-	public $RequireValidation = false;
+  public $RequireValidation = false;
 
-	protected function _index() {}
+  protected function _index() {
+  }
 
-	protected function before() {
-		application::app()->exclude_from_sitemap = true;
-		parent::before();
+  protected function before() {
+    application::app()->exclude_from_sitemap = true;
+    parent::before();
+  }
 
-	}
-
-	public function bootstrap( $type = 'css', $version = 4) {
+  public function bootstrap($type = 'css', $version = 4) {
     // \sys::logger( sprintf('<%s/%s> %s', $type, $version, __METHOD__));
 
-		if ( 'fonts' == $type) {
-      sys::serveBootStrap( $type, $version);
-
+    if ('fonts' == $type) {
+      sys::serveBootStrap($type, $version);
+    } elseif (4 == (int)$version) {
+      sys::serveBootStrap($type);
+    } elseif (5 == (int)$version && in_array($type, ['polyfill', 'css', 'js'])) {
+      sys::serveBootStrap5($type);
     }
-		elseif ( 4 == (int)$version) {
-			sys::serveBootStrap( $type);
+  }
 
-		}
-		elseif ( 5 == (int)$version && in_array( $type, ['polyfill','css','js'])) {
-			sys::serveBootStrap5( $type);
-
-		}
-
-	}
-
-	public function brayworth( $type = 'css', $p2 = '') {
-		if ( 'css' == $type) {
+  public function brayworth($type = 'css', $p2 = '') {
+    if ('css' == $type) {
       $files = [];
 
-      $lib ='dvc-4';
+      $lib = 'dvc-4';
       $_files = cssmin::$dvc4Files;
       // if ( sys::bootStrap_verion()->major >= 5) {
       //   $lib = 'dvc-5';
@@ -52,107 +46,88 @@ class assets extends Controller {
 
       // }
 
-			foreach( $_files as $f) {
-				$path = sprintf( '%s/../%s', __DIR__, $f);
-				if ( $_f = realpath( $path)) {
-					$key = basename( $_f);
-					$files[ $key] = $_f;
-
-				}
-
-			}
+      foreach ($_files as $f) {
+        $path = sprintf('%s/../%s', __DIR__, $f);
+        if ($_f = realpath($path)) {
+          $key = basename($_f);
+          $files[$key] = $_f;
+        }
+      }
 
       cssmin::viewcss([
-				'debug' => false,
-				'libName' => $lib,
-				'cssFiles' => $files,
-				'libFile' => config::tempdir()  . '_' . $lib . '.css'
+        'debug' => false,
+        'libName' => $lib,
+        'cssFiles' => $files,
+        'libFile' => config::tempdir()  . '_' . $lib . '.css'
 
-			]);
+      ]);
+    } elseif ('dopo' == $type) {
+      ob_start();
 
-		}
-		elseif ( 'dopo' == $type) {
-			ob_start();
+      $files = [];
+      foreach (jslib::$brayworthlibDOPOFiles as $f) {
+        $path = sprintf('%s/../%s', __DIR__, $f);
+        //~ sys::logger( sprintf( '%s', $path));
+        if ($_f = realpath($path)) {
+          $key = basename($_f);
+          $files[$key] = $_f;
+        }
+      }
 
-			$files = [];
-			foreach( jslib::$brayworthlibDOPOFiles as $f) {
-				$path = sprintf( '%s/../%s', __DIR__, $f);
-				//~ sys::logger( sprintf( '%s', $path));
-				if ( $_f = realpath( $path)) {
-					$key = basename( $_f);
-					$files[ $key] = $_f;
+      //~ $n = 0;
+      foreach ($files as $key => $path) {
+        //~ sys::logger( sprintf( "[%s] %s", $key, $path));
+        include_once $path;
+        print PHP_EOL;
+      }
 
-				}
+      $out = ob_get_contents();
+      ob_end_clean();
 
-			}
+      $minifier = new MatthiasMullie\Minify\JS;
+      $minifier->add($out);
+      $minified =  $minifier->minify();
 
-			//~ $n = 0;
-			foreach ( $files as $key => $path) {
-				//~ sys::logger( sprintf( "[%s] %s", $key, $path));
-				include_once $path;
-				print PHP_EOL;
+      Response::javascript_headers();
+      print $minified;
+    } else {
+      // sys::dump( \jslib::$brayworthlibFiles);
+      $files = [];
+      foreach (jslib::$brayworthlibFiles as $f) {
+        $path = sprintf('%s/../%s', __DIR__, $f);
+        // printf( '%s<br />', $path);
+        if ($_f = realpath($path)) {
+          $key = basename($_f);
+          $files[$key] = $_f;
+        }
+      }
 
-			}
+      if ($type == 'bundle') {
+        array_unshift($files, sprintf('%s/../js/%s', __DIR__, 'jquery-3.3.1.min.js'));
+      }
 
-			$out = ob_get_contents();
-			ob_end_clean();
+      // sys::dump( $files);
 
-			$minifier = new MatthiasMullie\Minify\JS;
-			$minifier->add( $out);
-			$minified =  $minifier->minify();
+      jslib::viewjs([
+        'debug' => false,
+        'libName' => 'brayworth',
+        'jsFiles' => $files,
+        'libFile' => config::tempdir()  . '_brayworth_has_dayjs.3.js'
 
-			Response::javascript_headers();
-			print $minified;
+      ]);
+    }
+  }
 
-		}
-		else {
-			// sys::dump( \jslib::$brayworthlibFiles);
-			$files = [];
-			foreach( jslib::$brayworthlibFiles as $f) {
-				$path = sprintf( '%s/../%s', __DIR__, $f);
-				// printf( '%s<br />', $path);
-				if ( $_f = realpath( $path)) {
-					$key = basename( $_f);
-					$files[ $key] = $_f;
+  public function fullcalendar($type = 'css') {
+    sys::serveFullcalendar($type);
+  }
 
-				}
-
-			}
-
-			if ( $type == 'bundle') {
-				array_unshift( $files, sprintf('%s/../js/%s', __DIR__, 'jquery-3.3.1.min.js'));
-
-			}
-
-			// sys::dump( $files);
-
-			jslib::viewjs([
-				'debug' => false,
-				'libName' => 'brayworth',
-				'jsFiles' => $files,
-				'libFile' => config::tempdir()  . '_brayworth_has_dayjs.3.js'
-
-			]);
-
-		}
-
-	}
-
-	public function fullcalendar( $type = 'css') {
-		sys::serveFullcalendar( $type);
-
-	}
-
-	public function jquery() {
-		if ( '3.4' == \config::$JQUERY_VERSION) {
-			\sys::serve( sprintf('%s/../js/%s', __DIR__, 'jquery-3.4.1.min.js'));
-
-		}
-		else {
-			\sys::serve( sprintf('%s/../js/%s', __DIR__, 'jquery-3.5.1.min.js'));
-
-		}
-
-	}
-
+  public function jquery() {
+    if ('3.4' == \config::$JQUERY_VERSION) {
+      \sys::serve(sprintf('%s/../js/%s', __DIR__, 'jquery-3.4.1.min.js'));
+    } else {
+      // \sys::serve(sprintf('%s/../js/%s', __DIR__, 'jquery-3.5.1.min.js'));
+      \sys::serve(sprintf('%s/../js/%s', __DIR__, 'jquery-3.6.0.min.js'));
+    }
+  }
 }
