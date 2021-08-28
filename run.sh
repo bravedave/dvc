744dceda5f150b0559c4ba3433f32e3437d5fdd3
Dave Bray <david@brayworth.com.au>
2021-08-28 18:07:42 +1000

    bootstrap updates

--- run.sh
+++ run.sh
@@ -2,41 +2,68 @@
 
 WD=`pwd`
 PORT=$[RANDOM%1000+1024]
+
 apache=`command -v httpd`
 
-cd tests/www
 
 if [[ "" == $apache ]]; then
+  cd tests/www
+
   php=php
   if [[ -x /usr/bin/php8 ]]; then php=php8; fi
 
   echo "this application is available at http://localhost:$PORT"
   $php -S localhost:$PORT _dvc.php
 
+  cd $WD
+
 else
-  data="`pwd`/../application/data"
+  data="`pwd`/tests/application/data"
   error_log="$data/error.log"
   access_log="$data/access.log"
   config="$data/httpd.conf"
-  [[ ! -f $error_log ]] || rm $error_log
-  [[ ! -f $access_log ]] || rm $access_log
-  if [[ ! -f $config ]]; then
-    cp $WD/httpd-minimal.conf $config
-    echo "<Directory `pwd`>" >>$config
-    echo "  AllowOverride all" >>$config
-    echo "  Require all granted" >>$config
-    echo "</Directory>" >>$config
+  pidFile="$data/httpd.pid"
+
+  if [ "$1" == "kill" ]; then
+    if [[ -f $pidFile ]]; then
+      kill `cat $pidFile`
+      if [[ -f $pidFile ]]; then
+        rm $pidFile
+
+      fi
+
+    fi
+
+  else
+
+    [[ ! -f $error_log ]] || rm $error_log
+    [[ ! -f $access_log ]] || rm $access_log
+    if [[ ! -f $config ]]; then
+      cp $WD/httpd-minimal.conf $config
+      echo "ErrorLogFormat \"[%t] %M\"" >>$config
+      echo "ErrorLog $error_log" >>$config
+      echo "CustomLog $access_log common" >>$config
+      echo "DocumentRoot `pwd`/tests/www" >>$config
+      echo "<Directory `pwd`/tests/www>" >>$config
+      echo "  AllowOverride all" >>$config
+      echo "  Require all granted" >>$config
+      echo "</Directory>" >>$config
+
+    fi
+
+    if [[ -f $pidFile ]] ; then
+      echo "running ..`cat $pidFile`"
+
+    else
+      echo "this application is available at http://localhost:$PORT"
+      httpd \
+        -f $config \
+        -c "Listen $PORT" \
+        -c "PidFile $data/httpd.pid"
+      tail -f $data/error.log
+
+    fi
 
   fi
 
-  echo "this application is available at http://localhost:$PORT"
-  httpd  -D FOREGROUND \
-    -f $config \
-    -c "DocumentRoot `pwd`" \
-    -c "Listen $PORT" \
-    -c "ErrorLog $error_log" \
-    -c "CustomLog $access_log common" \
-    -c "PidFile $data/httpd.pid"
-
 fi
-cd $WD
