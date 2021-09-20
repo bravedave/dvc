5f2410a578a808546dfe2e1dd47675cc1161e7e8
Dave Bray <david@brayworth.com.au>
2021-09-20 16:55:51 +1000

    add basic templating

--- src/dvc/template.php
+++ src/dvc/template.php
@@ -10,47 +10,52 @@
 
 namespace dvc;
 
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+
 class template {
-	protected $_template = '', $_css = [];
+  protected $_template = '', $_css = [];
 
-	public $title = '';
+  public $title = '';
 
-	function __construct( $filepath, $css = null) {
-		if ( (bool)$css)
-		 	$this->_css[] = file_get_contents( $css);
+  function __construct($filepath, $css = null) {
+    if ((bool)$css) {
+      $this->_css[] = file_get_contents($css);
+    }
 
-		$this->_template = file_get_contents( $filepath);
+    $this->_template = file_get_contents($filepath);
+  }
 
-	}
+  function css($path) {
+    if ((bool)$path) {
+      $this->_css[] = file_get_contents($path);
+    }
 
-	function css( $path) {
-		if ( (bool)$path)
-		 	$this->_css[] = file_get_contents( $path);
+    return ($this);  // chain
 
-		return ( $this);	// chain
+  }
 
-	}
+  function replace($var, $content) {
+    $this->_template = str_replace(sprintf('{{%s}}', $var), $content, $this->_template);
 
-	function replace($var, $content) {
-		$this->_template = str_replace( sprintf( '{{%s}}', $var), $content, $this->_template);
+    return ($this);  // chain
 
-		return ( $this);	// chain
+  }
 
-	}
-
-	function render() {
-		if ( count( $this->_css)) {
-			// create instance
-			$cssToInlineStyles = new TijsVerkoyen\CssToInlineStyles\CssToInlineStyles();
-			// output
-			return ( $cssToInlineStyles->convert( sprintf( '<html><head><title>%s</title></head><body>%s</body></html>', $this->title, $this->_template), implode( '', $this->_css)));
-
-		}
-		else {
-			return( $this->_template);
-
-		}
-
-	}
-
+  function render() {
+    if (count($this->_css)) {
+      // create instance
+      $cssToInlineStyles = new CssToInlineStyles;
+      // output
+      return $cssToInlineStyles->convert(
+        sprintf(
+          '<!DOCTYPE html><html lang="en"><head><title>%s</title></head><body>%s</body></html>',
+          $this->title,
+          $this->_template
+        ),
+        implode('', $this->_css)
+      );
+    } else {
+      return ($this->_template);
+    }
+  }
 }
