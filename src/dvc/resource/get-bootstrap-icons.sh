276f1e25c21102bf34de4cbe577ba02a2b7504eb
Dave Bray <david@brayworth.com.au>
2021-11-08 14:29:16 +1000

    moved bs icon support to composer

--- src/dvc/resource/get-bootstrap-icons.sh
+++ src/dvc/resource/get-bootstrap-icons.sh
@@ -3,6 +3,13 @@
 me=`basename "$0"`
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+if [ -d bootstrap-icons ]; then
+  echo "no longer included"
+  echo "should be removed .."
+  rm -fR bootstrap-icons
+fi
+exit 0
+
 echo "start : $me"
 
 [ -d bootstrap-icons.tmp ] || git clone https://github.com/twbs/icons/ bootstrap-icons.tmp
