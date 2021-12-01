ef1de2cfa939dd5dcccc1cda2a3cbd741a2eb159
Dave Bray <david@brayworth.com.au>
2021-12-01 12:01:34 +1000

    consolidete version checking

--- src/dao/_dbinfo.php
+++ src/dao/_dbinfo.php
@@ -11,28 +11,65 @@
 namespace dao;
 
 class _dbinfo extends _dao {
-	protected function checkDIR( $dir) {
-		\sys::logger( sprintf('<checking %s> %s', $dir, __METHOD__));
-		if ( $glob = glob( $dir . '/db/*.php')) {
-			foreach ( $glob as $f ) {
-				\sys::logger( sprintf('<checking => %s> %s', $f, __METHOD__));
-				include_once $f;
+  protected $_store = '';
 
-			}
+  protected function check() {
+    $this->checkDIR(__DIR__);
+  }
 
-		}
+  protected function checkDIR($dir) {
+    \sys::logger(sprintf('<checking %s> %s', $dir, __METHOD__));
+    if ($glob = glob($dir . '/db/*.php')) {
+      foreach ($glob as $f) {
+        \sys::logger(sprintf('<checking => %s> %s', $f, __METHOD__));
+        include_once $f;
+      }
+    }
+  }
 
-	}
+  protected function db_version_file() {
+    return implode(
+      DIRECTORY_SEPARATOR,
+      [
+        $this->_store,
+        'db_version.json'
 
-	protected function check() {
-		$this->checkDIR( __DIR__);
+      ]
+    );
+  }
 
-	}
+  public function __construct(\dvc\dbi $db = null, string $store = null) {
+    parent::__construct($db);
+    $this->_store = rtrim(!$store ? \config::dataPath() : $store, '/ ');
+  }
 
-	function dump( $verbose = true) {
-		$this->check();
-		if ( (bool)$verbose) $this->db->dump();
+  public $debug = false;
+  public function checkVersion(string $key, float $version): float {
 
-	}
+    $_version = 0;
+    $json = (object)[];
 
+    if (file_exists($store = $this->db_version_file())) {
+      $json = json_decode(file_get_contents($store));
+      $_version = (float)$json->{$key} ?? 0;
+    }
+
+    if ($_version < $version) {
+      if ( $this->debug) \sys::logger(sprintf('<%s> <needs update %s ? %s> %s', $store, $_version, $version, __METHOD__));
+      $this->dump($verbose = false);
+
+      $_version = $json->{$key} = $version;
+
+      file_put_contents($store, json_encode($json, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+      chmod($store, 0666);
+    } else {
+      if ( $this->debug) \sys::logger(sprintf('<%s> <up to date %s ? %s> %s', $store, $_version, $version, __METHOD__));
+    }
+    return $_version;
+  }
+
+  function dump($verbose = true) {
+    $this->check();
+    if ((bool)$verbose) $this->db->dump();
+  }
 }
