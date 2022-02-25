8d71e88365238f90a3df40ff8ccc4604013a5cc2
Dave Bray <david@brayworth.com.au>
2022-02-25 14:15:39 +1000

    update to ensure starts with alpha

--- src/dvc/js/_brayworth_.js
+++ src/dvc/js/_brayworth_.js
@@ -279,8 +279,12 @@ if (!window._brayworth_) {
 
   };
 
+  _.generateRandomInt = (min, max) => {
+    return Math.floor((Math.random() * (max + 1 - min)) + min);
+  };
+
   // https://dev.to/ovi/20-javascript-one-liners-that-will-help-you-code-like-a-pro-4ddc
-  _.randomString = () => Math.random().toString(36).slice(2);
+  _.randomString = () => 'abcdefghijklmnopqrstuvwxyz'.charAt(generateRandomInt(0, 25)) + Math.random().toString(36).slice(2)
 
   _.timer = () => {
     return (new function () {
