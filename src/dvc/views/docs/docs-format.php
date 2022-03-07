754d8ceb39a868434a3c3c1c873bc031f78058f7
Dave Bray <david@brayworth.com.au>
2022-03-07 11:29:32 +1000

    make h tags smaller

--- src/dvc/views/docs/docs-format.php
+++ src/dvc/views/docs/docs-format.php
@@ -12,6 +12,36 @@
     margin-top: 1rem;
     margin-bottom: .5rem;
   }
+
+  div[data-role="content-primary"]>.markdown-body>h1,
+  div[data-role="content-primary"]>.markdown-body>.h1 {
+    font-size: 1.8rem
+  }
+
+  div[data-role="content-primary"]>.markdown-body>h2,
+  div[data-role="content-primary"]>.markdown-body>.h2 {
+    font-size: 1.6rem
+  }
+
+  div[data-role="content-primary"]>.markdown-body>h3,
+  div[data-role="content-primary"]>.markdown-body>.h3 {
+    font-size: 1.4rem
+  }
+
+  div[data-role="content-primary"]>.markdown-body>h4,
+  div[data-role="content-primary"]>.markdown-body>.h4 {
+    font-size: 1.2rem
+  }
+
+  div[data-role="content-primary"]>.markdown-body>h5,
+  div[data-role="content-primary"]>.markdown-body>.h5 {
+    font-size: 1.1rem
+  }
+
+  h6,
+  .h6 {
+    font-size: 1rem
+  }
 </style>
 <script>
   $(document).ready(() => {
