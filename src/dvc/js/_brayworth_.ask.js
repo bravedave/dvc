a03ce42cf0e7601a45a49eb0e363013c082f4001
Dave Bray <david@brayworth.com.au>
2022-03-08 17:46:56 +1000

    added success alert

--- src/dvc/js/_brayworth_.ask.js
+++ src/dvc/js/_brayworth_.ask.js
@@ -19,52 +19,55 @@
     });
 
  * */
-( _ => {
+(_ => {
   _.ask = params => {
     let dlg = _.modal.template();
 
-    let options = _.extend({
-      beforeOpen: function() {
-        let modal = this;
-        $('.modal-title', modal).html( options.title);
-        $('.modal-body', modal).html( options.text);
+    let options = {
+      ...{
+        beforeOpen: function () {
+          let modal = this;
+          $('.modal-title', modal).html(options.title);
+          $('.modal-body', modal).html(options.text);
 
-        let footer = $('.modal-footer', modal);
-        $.each( options.buttons, ( key, j) => {
+          let footer = $('.modal-footer', modal);
+          $.each(options.buttons, (key, j) => {
             $('<button class="btn btn-light" type="button"></button>')
-            .html(key)
-            .appendTo(footer)
-            .on('click', e => j.call(modal, e));
+              .html(key)
+              .appendTo(footer)
+              .on('click', e => j.call(modal, e));
 
-        });
+          });
 
-      },
-      buttons: {},
-      headClass: 'text-white bg-dark',
-      onClose : e => {},
-      removeOnClose: true,
-      text: 'Question',
-      title: 'Topic',
+        },
+        buttons: {},
+        headClass: 'text-white bg-dark',
+        onClose: e => { },
+        removeOnClose: true,
+        text: 'Question',
+        title: 'Topic',
 
-    }, params);
+      }, ...params
+    };
 
     // console.log( options);
 
-    if ( options.removeOnClose) {
-      dlg.on('hidden.bs.modal', function(e) { $(this).remove(); });
+    if (options.removeOnClose) {
+      dlg.on('hidden.bs.modal', function (e) { $(this).remove(); });
 
     }
 
     $('.modal-header', dlg).addClass(options.headClass);
-    dlg.appendTo( 'body');
-    options.beforeOpen.call( dlg);
+    dlg.appendTo('body');
+    options.beforeOpen.call(dlg);
     dlg.modal('show');
 
     return dlg;	// a jQuery element
 
   }
 
-  _.ask.alert = p => _.ask(_.extend({ headClass: 'text-white bg-danger' }, p));
-  _.ask.warning = p => _.ask(_.extend({ headClass: 'text-white bg-warning' }, p));
+  _.ask.alert = p => _.ask({ ...{ headClass: 'text-white bg-danger', title: 'Alert' }, ...p });
+  _.ask.success = p => _.ask({ ...{ headClass: 'text-white bg-success', title: 'Success' }, ...p });
+  _.ask.warning = p => _.ask({ ...{ headClass: 'text-white bg-warning', title: 'Warning' }, ...p });
 
 })(_brayworth_);
