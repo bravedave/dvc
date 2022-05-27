ef6e15d68ca9da6d2456d4aadf740dd26aa8c377
Dave Bray <david@brayworth.com.au>
2022-05-28 09:44:23 +1000

    error from 100 years ago

--- src/dvc/Response.php
+++ src/dvc/Response.php
@@ -80,9 +80,8 @@ abstract class Response {
       }
     } else {
       return implode(PHP_EOL, [
-        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">',
-        '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">")'
-
+        '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
+        '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">'
       ]);
     }
   }
