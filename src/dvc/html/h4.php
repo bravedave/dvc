348b67bc112589557d5929ab685dc515817b31fc
Dave Bray <david@brayworth.com.au>
2022-07-19 15:52:49 +1000

    just pdfing along

--- src/dvc/html/h4.php
+++ src/dvc/html/h4.php
@@ -1,20 +1,17 @@
 <?php
 /*
-	David Bray
-	BrayWorth Pty Ltd
-	e. david@brayworth.com.au
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
 
-	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
-		http://creativecommons.org/licenses/by/4.0/
-
-	*/
-
-NameSpace dvc\html;
+namespace dvc\html;
 
 class h4 extends element {
-	function __construct( $content = NULL ) {
-		parent::__construct( 'h4', $content );
-
-	}
-
+  function __construct($content = null, $attributes = null) {
+    parent::__construct('h4', $content, $attributes);
+  }
 }
