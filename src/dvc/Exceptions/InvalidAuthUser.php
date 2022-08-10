3628ec9eb5ee1632af50263cf894a70159104f99
Dave Bray <david@brayworth.com.au>
2022-08-10 23:06:31 +1000

    missing exception

---
+++ src/dvc/Exceptions/InvalidAuthUser.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace dvc\Exceptions;
+
+class InvalidAuthUser extends Exception {
+  protected $_text = 'invalid user';
+}
--- src/dvc/controller/auth.php
+++ src/dvc/controller/auth.php
@@ -9,52 +9,46 @@
 */
 
 class auth extends Controller {
-	public $RequireValidation = false;
-	//~ public $debug = true;
+  public $RequireValidation = false;
+  //~ public $debug = true;
 
-	protected function before() {
-		application::app()->exclude_from_sitemap = true;
-		parent::before();
+  protected function before() {
+    application::app()->exclude_from_sitemap = true;
+    parent::before();
+  }
 
-	}
+  public function index() {
+    if ($this->getParam('code'))
+      $this->response();
+    else
+      $this->request();
+  }
 
-	public function index() {
-		if ( $this->getParam( 'code'))
-			$this->response();
-		else
-			$this->request();
+  public function request() {
+    if ($this->debug) sys::logger('auth/request');
 
-	}
+    if (dvc\auth::GoogleAuthEnabled()) {
+      $client = dvc\Google::client();
+      $url = $client->createAuthUrl();
+      Response::redirect($url);
+    } else {
+      throw new dvc\Exceptions\GoogleAuthNotEnabled;
+    }
+  }
 
-	public function request() {
-		if ( $this->debug) sys::logger( 'auth/request');
+  public function response() {
+    if (dvc\auth::GoogleAuthEnabled()) {
+      $client = \dvc\Google::client();
+      $client->authenticate($this->getParam('code'));
 
-		if ( dvc\auth::GoogleAuthEnabled()) {
-			$client = \dvc\Google::client();
-			$url = $client->createAuthUrl();
-			Response::redirect( $url);
+      dvc\Google::saveSession($client);
 
-		}
-		else {
-			throw new dvc\Exceptions\GoogleAuthNotEnabled;
+      $plus = dvc\Google::plus($client);
+      $me = $plus->people->get("me");
 
-		}
-
-	}
-
-	public function response() {
-		if ( dvc\auth::GoogleAuthEnabled()) {
-			$client = \dvc\Google::client();
-				$client->authenticate( $this->getParam( 'code'));
-
-				\dvc\Google::saveSession( $client);
-
-			$plus = dvc\Google::plus( $client);
-			$me = $plus->people->get("me");
-
-			if ( isset( $me->emails)) {
-				if ( (string)$me->emails[0]->value != '' ) {
-					/*
+      if (isset($me->emails)) {
+        if ((string)$me->emails[0]->value != '') {
+          /*
 					 * So here we are saying we accept any user that
 					 * bothers to identify with a valid google account
 					 * ----------------
@@ -63,38 +57,27 @@ class auth extends Controller {
 					 * this routing to say what the authenticated user
 					 * can or cannot access
 					 */
-					$oauth = new dvc\oauth();
-						$oauth->displayName = $me->displayName;
-						$oauth->Surname =  $me->name->familyName;
-						$oauth->GivenNames =  $me->name->givenName;
-						$oauth->email = $me->emails[0]->value;
-					\currentUser::sync( $oauth);
-					\session::set( 'DisplayName', $oauth->displayName);
-					\session::set( 'Email', $oauth->email);
-					\session::set( 'avatar', $me->image->url);
+          $oauth = new dvc\oauth();
+          $oauth->displayName = $me->displayName;
+          $oauth->Surname =  $me->name->familyName;
+          $oauth->GivenNames =  $me->name->givenName;
+          $oauth->email = $me->emails[0]->value;
+          currentUser::sync($oauth);
+          dvc\session::set('DisplayName', $oauth->displayName);
+          dvc\session::set('Email', $oauth->email);
+          dvc\session::set('avatar', $me->image->url);
 
-					\user::setGoogleFlag();
+          user::setGoogleFlag();
+        } else {
+          throw new dvc\Exceptions\InvalidAuthUser;
+        }
+      } else {
+        throw new dvc\Exceptions\InvalidAuthUser;
+      }
+    } else {
+      throw new dvc\Exceptions\GoogleAuthNotEnabled;
+    }
 
-				}
-				else {
-					throw new \Exceptions\InvalidAuthUser;
-
-				}
-
-			}
-			else {
-				throw new \Exceptions\InvalidAuthUser;
-
-			}
-
-		}
-		else {
-			throw new dvc\Exceptions\GoogleAuthNotEnabled;
-
-		}
-
-		\Response::redirect();
-
-	}
-
+    \Response::redirect();
+  }
 }
