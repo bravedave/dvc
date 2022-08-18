975ff115bf90394a226eaa9d72649ee6b0a79395
Dave Bray <david@brayworth.com.au>
2022-08-19 07:49:06 +1000

    going to look at stimulus

--- src/dvc/resource/get-stimulus.sh
+++ src/dvc/resource/get-stimulus.sh
@@ -3,7 +3,7 @@
 me=`basename "$0"`
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-curl -O https://unpkg.com/@hotwired/stimulus/dist/stimulus.js
+curl -OL https://unpkg.com/@hotwired/stimulus/dist/stimulus.js
 cp stimulus.js ../js/
 rm stimulus.js
 
