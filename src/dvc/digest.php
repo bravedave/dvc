9be77fa72bf802ab56e440e2a416f102ed58af21
Dave Bray <david@brayworth.com.au>
2022-08-20 07:24:53 +1000

    format

--- src/dvc/digest.php
+++ src/dvc/digest.php
@@ -14,74 +14,70 @@ class digest {
   var $digestParts = [];
   var $realm = '';
 
-  function authorized( $A1) {
-    return ( $this->authorised( $A1));
-
+  function authorized($A1) {
+    return ($this->authorised($A1));
   }
 
-  function authorised( $A1) {
-    $A2 = md5( $_SERVER['REQUEST_METHOD'] . ':' . $this->digestParts['uri'] );
-    $valid_response = md5( sprintf( '%s:%s:%s:%s:%s:%s',
-      $A1,
-      $this->digestParts['nonce'],
-      $this->digestParts['nc'],
-      $this->digestParts['cnonce'],
-      $this->digestParts['qop'],
-      $A2)
+  function authorised($A1) {
+    $A2 = md5($_SERVER['REQUEST_METHOD'] . ':' . $this->digestParts['uri']);
+    $valid_response = md5(
+      sprintf(
+        '%s:%s:%s:%s:%s:%s',
+        $A1,
+        $this->digestParts['nonce'],
+        $this->digestParts['nc'],
+        $this->digestParts['cnonce'],
+        $this->digestParts['qop'],
+        $A2
+      )
 
     );
 
-    return ( $this->digestParts['response'] == $valid_response);
-
+    return ($this->digestParts['response'] == $valid_response);
   }
 
   protected function getDigest() {
     // return $this->request->getHeader('Authorization');
-    return ( $_SERVER['PHP_AUTH_DIGEST']);
-
+    return ($_SERVER['PHP_AUTH_DIGEST']);
   }
 
   /**
-  * Returns digest hash for a @key / @secret.
-  *
-  * @return string|null
-  */
-	function getDigestHash($key, $secret) {
-			return ( md5( sprintf( '%s:%s:%s', $key, $this->realm, $secret)));
+   * Returns digest hash for a @key / @secret.
+   *
+   * @return string|null
+   */
+  function getDigestHash($key, $secret) {
+    return (md5(sprintf('%s:%s:%s', $key, $this->realm, $secret)));
 
-		return NULL;
-
-	}
+    return NULL;
+  }
 
   /**
-  * Returns the username for the request
-  *
-  * @return string
-  */
+   * Returns the username for the request
+   *
+   * @return string
+   */
   function getUsername() {
     return $this->digestParts['username'];
-
   }
 
   protected function hasHeader() {
-    return ( !(empty($_SERVER['PHP_AUTH_DIGEST'])));
-
+    return (!(empty($_SERVER['PHP_AUTH_DIGEST'])));
   }
 
   function requireAuth() {
     header('HTTP/1.1 401 Unauthorized');
-    header( sprintf( 'WWW-Authenticate: Digest realm="%s",qop="auth",nonce="%s",opaque="%s"', $this->realm, uniqid(), md5($this->realm)));
+    header(sprintf('WWW-Authenticate: Digest realm="%s",qop="auth",nonce="%s",opaque="%s"', $this->realm, uniqid(), md5($this->realm)));
 
-    throw new \Exception( "No 'Authorization: Digest' header found.");
-
+    throw new \Exception("No 'Authorization: Digest' header found.");
   }
 
   /**
-  * Parses the different pieces of the digest string into an array.
-  *
-  * @param string $digest
-  * @return false|array
-  */
+   * Parses the different pieces of the digest string into an array.
+   *
+   * @param string $digest
+   * @return false|array
+   */
   protected function parseDigest($digest) {
     // protect against missing data
     $needed_parts = ['nonce' => 1, 'nc' => 1, 'cnonce' => 1, 'qop' => 1, 'username' => 1, 'uri' => 1, 'response' => 1];
@@ -92,20 +88,16 @@ class digest {
     foreach ($matches as $m) {
       $data[$m[1]] = $m[2] ? $m[2] : $m[3];
       unset($needed_parts[$m[1]]);
-
     }
 
     return $needed_parts ? false : $data;
-
   }
 
-  function __construct( $realm = 'DVC') {
+  function __construct($realm = 'DVC') {
     $this->realm = $realm;
 
     $this->hasHeader() ?
-      $this->digestParts = $this->parseDigest( $this->getDigest()) :
+      $this->digestParts = $this->parseDigest($this->getDigest()) :
       $this->requireAuth();
-
   }
-
 }
