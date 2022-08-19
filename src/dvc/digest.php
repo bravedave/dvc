9807be7940532aac96f3b4bcbe8acc99289f3101
Dave Bray <david@brayworth.com.au>
2022-08-20 09:07:36 +1000

    bring up to date

--- src/dvc/digest.php
+++ src/dvc/digest.php
@@ -6,70 +6,54 @@
  *
  * MIT License
  *
+ * This works, but digest auth is not recommended,
+ * because you have to be able to descrypt the password
+ *
+ * example:
+ class home extends dvc\Controller {
+  protected function authorize() {
+
+		// issue a digest challenge
+		$users = [
+			'admin' => 'password'
+		];
+
+		$digest = new dvc\digest;
+		if ($user = $digest->getUsername()) {
+			if (isset($users[$user])) {
+				if ($authorised = $digest->authorised(md5(sprintf('%s:%s:%s', $user, $digest->realm, $users[$user])))) {
+
+          // ok to that passed the yes, do whatever you need to do to set them as logged on
+
+          die('valid');
+
+				} else {
+					die('invalid ' . $user);
+				}
+			} else {
+				$digest->requireAuth();
+			}
+		} else {
+			$digest->requireAuth();
+		}
+	}
+}
+
 */
 
 namespace dvc;
 
 class digest {
-  var $digestParts = [];
-  var $realm = '';
-
-  function authorized($A1) {
-    return ($this->authorised($A1));
-  }
-
-  function authorised($A1) {
-    $A2 = md5($_SERVER['REQUEST_METHOD'] . ':' . $this->digestParts['uri']);
-    $valid_response = md5(
-      sprintf(
-        '%s:%s:%s:%s:%s:%s',
-        $A1,
-        $this->digestParts['nonce'],
-        $this->digestParts['nc'],
-        $this->digestParts['cnonce'],
-        $this->digestParts['qop'],
-        $A2
-      )
-
-    );
-
-    return ($this->digestParts['response'] == $valid_response);
-  }
+  public $digestParts = [];
+  public $realm = '';
 
   protected function getDigest() {
     // return $this->request->getHeader('Authorization');
     return ($_SERVER['PHP_AUTH_DIGEST']);
   }
 
-  /**
-   * Returns digest hash for a @key / @secret.
-   *
-   * @return string|null
-   */
-  function getDigestHash($key, $secret) {
-    return (md5(sprintf('%s:%s:%s', $key, $this->realm, $secret)));
-
-    return NULL;
-  }
-
-  /**
-   * Returns the username for the request
-   *
-   * @return string
-   */
-  function getUsername() {
-    return $this->digestParts['username'];
-  }
-
   protected function hasHeader() {
     return (!(empty($_SERVER['PHP_AUTH_DIGEST'])));
-  }
-
-  function requireAuth() {
-    header('HTTP/1.1 401 Unauthorized');
-    header(sprintf('WWW-Authenticate: Digest realm="%s",qop="auth",nonce="%s",opaque="%s"', $this->realm, uniqid(), md5($this->realm)));
-
-    throw new \Exception("No 'Authorization: Digest' header found.");
   }
 
   /**
@@ -93,11 +77,65 @@ class digest {
     return $needed_parts ? false : $data;
   }
 
-  function __construct($realm = 'DVC') {
+  public function __construct($realm = 'DVC') {
     $this->realm = $realm;
 
     $this->hasHeader() ?
       $this->digestParts = $this->parseDigest($this->getDigest()) :
       $this->requireAuth();
+  }
+
+  public function authorized($A1) {
+    return ($this->authorised($A1));
+  }
+
+  public function authorised($A1) {
+    $A2 = md5($_SERVER['REQUEST_METHOD'] . ':' . $this->digestParts['uri']);
+    $valid_response = md5(
+      sprintf(
+        '%s:%s:%s:%s:%s:%s',
+        $A1,
+        $this->digestParts['nonce'],
+        $this->digestParts['nc'],
+        $this->digestParts['cnonce'],
+        $this->digestParts['qop'],
+        $A2
+      )
+
+    );
+
+    return ($this->digestParts['response'] == $valid_response);
+  }
+
+  /**
+   * Returns digest hash for a @key / @secret.
+   *
+   * @return string|null
+   */
+  function getDigestHash($key, $secret) {
+    return (md5(sprintf('%s:%s:%s', $key, $this->realm, $secret)));
+
+    return NULL;
+  }
+
+  /**
+   * Returns the username for the request
+   *
+   * @return string
+   */
+  function getUsername() {
+    return $this->digestParts['username'];
+  }
+
+  public function requireAuth() {
+    header('HTTP/1.1 401 Unauthorized');
+    header(sprintf(
+      'WWW-Authenticate: Digest realm="%s",qop="auth",nonce="%s",opaque="%s"',
+      $this->realm,
+      uniqid(),
+      md5($this->realm)
+    ));
+
+    throw new \Exception("No 'Authorization: Digest' header found.");
   }
 }
