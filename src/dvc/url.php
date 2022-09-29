672a1aebfc6507556147ac5d4b90149540d35689
Dave Bray <david@brayworth.com.au>
2022-09-29 18:01:23 +1000

    remove bug when run in subdir

--- src/dvc/url.php
+++ src/dvc/url.php
@@ -16,33 +16,45 @@ abstract class url {
   static public $PROTOCOL;
 
   static function tostring(string $url = '', bool $protocol = false): string {
+
     if ($protocol) {
-      return (implode([self::$PROTOCOL, self::$URL, $url]));
+
+      return implode([self::$PROTOCOL, self::$URL, $url]);
     } else {
-      return (implode([self::$URL, $url]));
+
+      return implode([self::$URL, $url]);
     }
   }
 
   static function write(string $url = '', bool $protocol = false) {
+
     print self::tostring($url, $protocol);
   }
 
   static function swrite(string $url = '', bool $protocol = false): string {
-    return (self::tostring($url, $protocol));
+
+    return self::tostring($url, $protocol);
   }
 
   static function init() {
     if (isset(self::$URL)) return;
 
     if (!(defined('URL'))) {
+
       if (isset($_SERVER['SERVER_SOFTWARE'])) {
+
         if (preg_match('@^PHP@', $_SERVER['SERVER_SOFTWARE'])) {
+
           if (application::use_full_url) {
+
             if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] != 80)
+
               define('URL', sprintf('//localhost:%s/', $_SERVER['SERVER_PORT']));
             else
+
               define('URL', '//localhost/');
           } else {
+
             define('URL', '/');
           }
         }
@@ -50,8 +62,8 @@ abstract class url {
     }
 
     if (!(defined('URL') && defined('URL_APPLICATION'))) {
-      $server = '';
-      if (isset($_SERVER['SERVER_NAME'])) $server = strtolower($_SERVER['SERVER_NAME']);
+
+      $server = strtolower($_SERVER['SERVER_NAME'] ?? '');
       $server = preg_replace('@\/$@', '', $server);
 
       $script = '';
@@ -60,24 +72,29 @@ abstract class url {
 
       $port = '';
       if (preg_match('@^Apache@', $_SERVER['SERVER_SOFTWARE'] ?? '')) {
+
         if (application::use_full_url) {
-          if (isset($_SERVER['SERVER_PORT']) && !in_array($_SERVER['SERVER_PORT'], [80, 443])) {
-            // \sys::logger(sprintf('<%s:%s> %s', $_SERVER['SERVER_SOFTWARE'], $_SERVER['SERVER_PORT'], __METHOD__));
+
+          if (!in_array($_SERVER['SERVER_PORT'] ?? 80, [80, 443])) {
+
             $port = ':' . $_SERVER['SERVER_PORT'];
           }
         }
       }
 
-      if (!(defined('URL_APPLICATION'))) define('URL_APPLICATION', '//' . $server . $script . $port . '/');
+      if (!(defined('URL_APPLICATION'))) define('URL_APPLICATION', sprintf('//%s%s%s/', $server, $port, $script));
 
       if (!(defined('URL'))) {
+
         if (application::use_full_url) {
+
           $script = preg_replace('@/application$@', '', $script);
-          define('URL', '//' . $server . $script . $port . '/');
+          define('URL', sprintf('//%s%s%s/', $server, $port, $script));
           // \sys::logger( sprintf( 'defining URL as %s - %s', $server, $script ), 3 );
 
         } else {
-          define('URL', '/');
+
+          define('URL', $script . '/');
         }
       }
     }
@@ -85,8 +102,11 @@ abstract class url {
     self::$URL = URL;
     self::$HOME = URL;
 
-    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ||
-      (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443)) ? "https:" : "http:";
+    $protocol = (
+      ($_SERVER['HTTPS'] ?? 'off') !== 'off'
+      ||
+      (($_SERVER['SERVER_PORT'] ?? 0) == 443)
+    ) ? "https:" : "http:";
 
     self::$PROTOCOL = $protocol;
   }
