5c05ad1203be99c3ece944c4c4dd02dbbf6666c9
Dave Bray <david@brayworth.com.au>
2022-09-29 18:00:23 +1000

    refactor

--- tests/www/_mvp.php
+++ tests/www/_mvp.php
@@ -13,11 +13,9 @@
 	if you do, check first and exit if it's a public resource - Serve that instead
 	 */
 if (preg_match('/\.(?:png|ico|jpg|jpeg|gif|css|js)$/', $_SERVER['REQUEST_URI'])) {
-  if (file_exists(trim($_SERVER['REQUEST_URI'], ' /\\')))
-    return false;    // serve the requested resource as-is.
 
+  if (file_exists(trim($_SERVER['REQUEST_URI'], ' /\\'))) return false;    // serve the requested resource as-is.
 }
 
 require __DIR__ . '/../../vendor/autoload.php'; // load the autoloader
-
 application::run(); // run the application
