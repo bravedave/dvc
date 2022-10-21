b77ceb6a2c302bf4148fc24b23a7fdf2e2f548e9
Dave Bray <david@brayworth.com.au>
2022-10-21 14:23:35 +1000

    bugs

--- src/dvc/Exceptions/Exception.php
+++ src/dvc/Exceptions/Exception.php
@@ -11,25 +11,24 @@
 namespace dvc\Exceptions;
 
 class Exception extends \Exception {
-	protected $_text = false;
+  protected $_text = false;
 
-	public function __construct($message = null, $code = 0, \Exception $previous = null) {
+  public function __construct($message = null, $code = 0, \Exception $previous = null) {
 
-		if ( !$this->_text)
-			$this->_text = '';
+    if (!$this->_text) $this->_text = '';
 
-		$sContext = str_replace('\\', '-', get_class($this)).' ('. basename($this->getFile()).' ~ '.$this->getLine().')';
-		if ( $this->_text == '')
-			$this->_text = $sContext;
-		else
-			$this->_text = $sContext . ' :: ' . $this->_text;
+    $sContext = str_replace('\\', '-', get_class($this)) . ' (' . basename($this->getFile()) . ' ~ ' . $this->getLine() . ')';
+    if ($this->_text == '') {
 
-		if ( 0 !== strlen( $message))
-			$this->_text .= ' : ' . (string)$message;
+      $this->_text = $sContext;
+    } else {
 
-		// make sure everything is assigned properly
-		parent::__construct( $this->_text, $code, $previous);
+      $this->_text = $sContext . ' :: ' . $this->_text;
+    }
 
-	}
+    if ($message) $this->_text .= ' : ' . (string)$message;
 
+    // make sure everything is assigned properly
+    parent::__construct($this->_text, $code, $previous);
+  }
 }
