a74042c1af06ae9cd623f6d25ba13012d22f0dfa
David Bray <david@brayworth.com.au>
2022-11-20 01:51:56 +0000

    syntax was probably incorrect

--- src/dvc/js/_brayworth_.extend.js
+++ src/dvc/js/_brayworth_.extend.js
@@ -9,7 +9,7 @@
 /*jshint esversion: 6 */
 
 (_ => {
-  _.extend = (...arguments) => {
+  _.extend = args => {
     let ext = {};
     let deep = false;
     let i = 0;
@@ -17,36 +17,35 @@
 
     // Check if a deep merge
     if ('[object Boolean]' === Object.prototype.toString.call(arguments[0])) {
+
       deep = arguments[0];
       i++;
-
     }
 
     let f = obj => {
+
       // Merge the object into the ext object
       for (let prop in obj) {
+
         if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+
           // If deep merge and property is an object, merge properties
           if (deep && Object.prototype.toString.call(obj[prop]) === '[object Object]') {
-            ext[prop] = _.extend(true, ext[prop], obj[prop]);
 
+            ext[prop] = _.extend(true, ext[prop], obj[prop]);
           }
           else {
+
             ext[prop] = obj[prop];
-
           }
-
         }
-
       }
-
     };
 
     // Loop through each object and conduct a merge
     for (; i < length; i++) f(arguments[i]);
 
     return ext;
-
   };
 
 })(_brayworth_);
