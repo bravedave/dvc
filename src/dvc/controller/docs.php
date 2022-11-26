fb662a1df1e8f83f10940006ab1356ce1e5b76d7
Dave Bray <david@brayworth.com.au>
2022-11-27 07:12:46 +1000

    add documentation path

--- src/dvc/controller/docs.php
+++ src/dvc/controller/docs.php
@@ -11,6 +11,7 @@
 namespace dvc\controller;
 
 use config, Controller, sys;
+use strings;
 
 class docs extends Controller {
   protected $RequireValidation = config::lockdown;
@@ -42,18 +43,23 @@ class docs extends Controller {
   }
 
   protected function _index($view = 'index.md') {
-    if (!$view) $view = 'index.md';
 
     // $this->debug = true;
 
+    if (!$view) $view = 'index.md';
     if ($this->debug) sys::logger(sprintf('<%s> %s', $view, __METHOD__));
+
     if (preg_match('@\.(png|jpg)$@', $view) && $_img = $this->_hasImage($view)) {
+
       sys::serve($_img);
     } else {
+
       $contents = ['contents'];
       if ($this->hasView($_c = sprintf('%s-contents', $view))) {
+
         $contents = $_c;
       } else if (strpos($view, '-') !== false) {
+
         if ($this->hasView($_c = sprintf('%s-contents', preg_replace('/-.*/', '', $view)))) {
           $contents = $_c;
         }
@@ -70,10 +76,14 @@ class docs extends Controller {
         'title' => $this->title = sprintf('Docs - %s', ucwords($view)),
         'primary' => $primary,
         'secondary' => $contents,
-
+        'data' => (object)[
+          'searchFocus' => false,
+          'pageUrl' => rtrim( strings::url($this->route), '/') . '/' . $view == 'index.md' ? '' : $view
+        ]
       ];
 
       if (config::$SYNTAX_HIGHLIGHT_DOCS) {
+
         // '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/default.min.css">'
         $render['css'] = [
           '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/github-gist.min.css">'
@@ -83,7 +93,6 @@ class docs extends Controller {
         $render['scripts'] = [
           '<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>',
           '<script>hljs.initHighlightingOnLoad();</script>'
-
         ];
       }
 
