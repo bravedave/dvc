59f5a5dcbf22e033f24013053401ef4689a05c3f
Dave Bray <david@brayworth.com.au>
2023-01-09 18:28:45 +1000

    make logging standalone

--- src/dvc/logger.php
+++ src/dvc/logger.php
@@ -12,8 +12,14 @@ namespace dvc;
 
 abstract class logger {
 
-  public static function info(string $msg): void {
+  public static function info(array|string $msg): void {
 
-    error_log($msg, 0);
+    if (is_array($msg)) {
+
+      array_walk($msg, fn ($m) => error_log($m, 0));
+    } else {
+
+      error_log($msg, 0);
+    }
   }
 }
