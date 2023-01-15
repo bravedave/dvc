659262c66de96511c325c0e1f46aa4dd4bbcba43
Dave Bray <david@brayworth.com.au>
2023-01-16 09:00:57 +1000

    bs v4 => v5 conversions

--- src/dvc/js/_brayworth_.hourglass.js
+++ src/dvc/js/_brayworth_.hourglass.js
@@ -23,7 +23,7 @@
               <div class="modal-content">
                 <div class="modal-body">
                   <div class="d-flex text-center py-2">
-                    ${!!msg ? `<h1 class="py-0 ${bv < 5 ? 'mr' : 'me'}-auto">${msg}</h1>` : ''}
+                    ${!!msg ? `<h1 class="py-0 my-1 ${bv < 5 ? 'mr' : 'me'}-auto">${msg}</h1>` : ''}
                     <i class="ml-auto spinner-border my-2" style="width: 3rem; height: 3rem;"></i>
                   </div>
                 </div>
