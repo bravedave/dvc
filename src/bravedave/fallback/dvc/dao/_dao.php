289c0bb079b3b86835722511cb8aac2273eb2c77
Dave Bray <david@brayworth.com.au>
2023-02-10 07:49:52 +1000

    quieten down

--- src/bravedave/fallback/dvc/dao/_dao.php
+++ src/bravedave/fallback/dvc/dao/_dao.php
@@ -15,9 +15,9 @@ use bravedave\dvc\logger;
 
 abstract class _dao extends bravedave\dvc\dao {
 
-  public function __construct(bravedave\dvc\db $db = null) {
+  // public function __construct(bravedave\dvc\db $db = null) {
 
-    logger::deprecated(sprintf('deprecated : use bravedave\dvc\dao : %s', get_class($this)), 2);
-    parent::__construct($db);
-  }
+  //   logger::deprecated(sprintf('deprecated : use bravedave\dvc\dao : %s', get_class($this)), 2);
+  //   parent::__construct($db);
+  // }
 }
