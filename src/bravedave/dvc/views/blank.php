3b9407d0a10977a5816d6a6aca1b272eedb7bf8b
Dave Bray <david@brayworth.com.au>
2023-02-12 09:52:14 +1000

    moving to PS-4

---
+++ src/bravedave/dvc/views/blank.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  David Bray
+  BrayWorth Pty Ltd
+  e. david@brayworth.com.au
+
+  MIT License
+
+	DO NOT change this file
+	Copy it to <application>/app/views/ and modify it there
+*/	?>
+
+	<div>&nbsp;</div>
---
+++ src/bravedave/dvc/views/footer-empty.php
@@ -0,0 +1,8 @@
+<?php
+/*
+  David Bray
+  BrayWorth Pty Ltd
+  e. david@brayworth.com.au
+
+  MIT License
+*/
