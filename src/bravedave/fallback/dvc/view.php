9e5612eef7e14289ee149c35c875e17289eb3b6a
Dave Bray <david@brayworth.com.au>
2023-02-12 10:57:32 +1000

    moving to PS-4

---
+++ src/bravedave/fallback/dvc/view.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace dvc;
+
+use bravedave;
+
+class view extends bravedave\dvc\view {
+}
