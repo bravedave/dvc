cceb18d7ce721d2afbc91105021c69ff4983c558
Dave Bray <david@brayworth.com.au>
2023-02-13 18:01:45 +1000

    fixed location of icons

--- src/resource/get-bootstrap-icons.sh
+++ src/resource/get-bootstrap-icons.sh
@@ -15,5 +15,6 @@ composer req --working-dir=bootstrap_icons twbs/bootstrap-icons
 # cd bootstrap_icons
 targetDir="../bravedave/dvc/css/bootstrap-icons/"
 rsync -arR --delete bootstrap_icons/vendor/twbs/bootstrap-icons/font/./ $targetDir
+rsync -arR --delete bootstrap_icons/vendor/twbs/bootstrap-icons/icons/./ $targetDir
 
 echo "done : $me"
