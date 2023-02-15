7543887f5ba54263c6bfd94963faeea58d184073
Dave Bray <david@brayworth.com.au>
2023-02-15 13:07:45 +1000

    add a placeholder

--- src/bravedave/dvc/js/placeholders.js
+++ src/bravedave/dvc/js/placeholders.js
@@ -13,7 +13,7 @@
 
     if ('TABLE' == String(this[0].nodeName)) {
 
-      _brayworth_.table.placeholders(this);
+      _brayworth_.table._placeholders_(this);
     } else {
 
       console.log(`cannot placeholder : ${this[0].nodeName}`);
