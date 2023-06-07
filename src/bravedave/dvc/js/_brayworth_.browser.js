f5ea55fa681e24d828cd50743594943eeed65fed
Dave Bray <david@brayworth.com.au>
2023-06-07 21:53:33 +0100

    Update _brayworth_.browser.js

--- src/bravedave/dvc/js/_brayworth_.browser.js
+++ src/bravedave/dvc/js/_brayworth_.browser.js
@@ -19,8 +19,7 @@
     // https://stackoverflow.com/questions/57765958/how-to-detect-ipad-and-ipad-os-version-in-ios-13-and-up/57924983#57924983
 
     _.browser.isIPad = (navigator.userAgent.match(/(iPad)/) /* iOS pre 13 */ ||
-            (navigator.platform === 'MacIntel' && navigator.maxTouchPoints >
-            1) /* iPad OS 13 */ );
+        (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) /* iPad OS 13 */ );
         
 	_.browser.isChromeOniOS = _.browser.isIPhone && ua.indexOf('CriOS') > -1;
 	_.browser.isMobileDevice = _.browser.isIPhone || _.browser.isIPad || _.browser.isAndroid;
