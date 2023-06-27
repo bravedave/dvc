fe797640153465260b9087e7cc471d454a88e86f
Dave Bray <david@brayworth.com.au>
2023-06-27 14:57:37 +1000

    reduce impact

--- src/bravedave/dvc/js/_brayworth_.hourglass.js
+++ src/bravedave/dvc/js/_brayworth_.hourglass.js
@@ -20,7 +20,7 @@
 
         _me.h = $(`<div class="modal" tabindex="-1">
             <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content bg-transparent shadow-none">
+              <div class="modal-content bg-transparent border-0 shadow-none">
                 <div class="modal-body">
                   <div class="d-flex text-center py-2">
                     ${!!msg ? `<h1 class="py-0 my-1 ${bv < 5 ? 'mr' : 'me'}-auto">${msg}</h1>` : ''}
