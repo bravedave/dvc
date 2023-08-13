b9559def21020e2c5e634f83dbd014cbea613990
David Bray <david@brayworth.com.au>
2023-08-13 07:23:22 +0000

    continue refactor

--- src/bravedave/dvc/js/autofill.js
+++ src/bravedave/dvc/js/autofill.js
@@ -14,14 +14,16 @@
  *		the response will be parsed in the fashion [<jsonObject>.label]
  * */
 (($, _) => {
+
   $.fn.autofill = function (params) {
+
     if ('string' == typeof params) {
+
       if ('destroy' == params) {
+
         this.trigger('destroy-autofill');
         return this;
-
       }
-
     }
 
     let options = {
@@ -42,10 +44,7 @@
     // console.log(options);
     let list = $('<ul class="list-group" style="position: absolute; left: 0; z-index: 5; width: 100%;"></ul>');
 
-    if (!(options.appendTo instanceof jQuery)) {
-      options.appendTo = $(options.appendTo);
-
-    }
+    if (!(options.appendTo instanceof jQuery)) options.appendTo = $(options.appendTo);
 
     options.wrapper.append(list).appendTo(options.appendTo);
 
@@ -57,139 +56,141 @@
       _initialized: false,
 
       activate: function (item) {
+
         if (this.current) {
+
           this.current.removeClass('active');
           this.current = false;
-
         }
 
         this.current = $(item);
         this.current.addClass('active');
-
       },
 
       deactivate: function (item) {
 
         let _item = $(item);
-        if (_item.hasClass('active')) {
-          _item.removeClass('active');
-
-        }
+        if (_item.hasClass('active')) _item.removeClass('active');
 
         if (this.current && !this.current.hasClass('active')) {
+
           this.current = false; // there is no item active
-
         }
-
       },
 
       up: function () {
+
         let items = this.items();
         if (items.length > 0) {
+
           let item = -1;
           $.each(items, (i, el) => {
+
             if ($(el).hasClass('active')) {
+
               item = i;
-              return (true); // break;
-
+              return true; // break;
             }
-
           });
 
           if (item < 0) {
-            this.activate(items[items.length - 1]);
 
+            this.activate(items[items.length - 1]);
           } else {
+
             item--;
             this.activate(items[item < 0 ? items.length - 1 : item]);
-
           }
-
         }
-
       },
 
       down: function () {
+
         let items = this.items();
         if (items.length > 0) {
+
           let item = -1;
           $.each(items, (i, el) => {
+
             if ($(el).hasClass('active')) {
+
               item = i;
-              return (true); // break
-
+              return true; // break
             }
-
           });
 
           if (item < 0) {
-            this.activate(items[0]);
 
+            this.activate(items[0]);
           } else {
+
             item++;
             this.activate(items[item > items.length - 1 ? 0 : item]);
-
           }
-
         }
-
       },
 
       selectitem: function (e) {
+
         keyMove.activate(this);
 
         let item = $(this).data('item');
         _me.val(!!item.value ? item.value : (!!item.label ? item.label : item));
         if (!!e.stopPropagation) {
+
           // this stops the click going through to an underlying element
           e.stopPropagation();
           e.preventDefault();
-
         }
 
         keyMove.clear();
-        if ('function' == typeof options.select) options.select(e, {
-          item: item
-        });
+        if ('function' == typeof options.select) {
 
+          options.select(e, {
+            item: item
+          });
+        }
       },
 
-      select: function (event) {
+      select: function (e) {
+
         let items = this.items();
         if (items.length > 0) {
+
           let item = -1;
           $.each(items, (i, el) => {
+
             if ($(el).hasClass('active')) {
+
               item = i;
               return (false);
-
             }
-
           });
 
           if (item > -1) {
-            this.selectitem.call(items[item], event);
 
-          } else if (!!event && (13 == event.keyCode || 9 == event.keyCode)) {
+            this.selectitem.call(items[item], e);
+          } else if (!!e && (13 == e.keyCode || 9 == e.keyCode)) {
+
             if (options.activateOnEnter || options.autoFocus) {
-              this.selectitem.call(items[0], event);
 
+              this.selectitem.call(items[0], e);
             }
-
           }
-
         }
-
       },
 
       clear: function () {
+
         list.html('');
         this.current = false;
         return this;
       },
 
       init: function () {
+
         if (this._initialized) return;
+
         /**
          * initialise placement of the list item
          *
@@ -202,8 +203,8 @@
 
         /*-- --[ position exactly where ? ]-- --*/
         if ('static' == options.appendTo.css('position')) {
-          options.appendTo.css('position', 'relative');
 
+          options.appendTo.css('position', 'relative');
         }
 
         let _mePos = _me.offset();
@@ -217,9 +218,7 @@
         childOffset.width = `calc(100% - ${childOffset.left}px)`;
         options.wrapper.css(childOffset);
         /*-- --[ position exactly where ? ]-- --*/
-
       }
-
     };
 
     let lastVal = '';
@@ -228,6 +227,7 @@
 
     this
       .one('destroy-autofill', () => {
+
         $(this)
           .off('keydown.autofill')
           .off('keypress.autofill')
@@ -236,94 +236,101 @@
           .off('blur.autofill');
 
         $(this).siblings('.autofill-wrapper').remove();
-
       })
       .on('focus.autofill', () => {
+
         if (blurTimeOut) {
-          window.clearTimeout(blurTimeOut);
+
+          clearTimeout(blurTimeOut);
           blurTimeOut = false;
-
         }
-
       })
       .on('blur.autofill', () => {
 
         if (blurTimeOut) {
-          window.clearTimeout(blurTimeOut);
-          blurTimeOut = false;
 
+          clearTimeout(blurTimeOut);
+          blurTimeOut = false;
         }
 
-        blurTimeOut = window.setTimeout(() => {
+        blurTimeOut = setTimeout(() => {
+
           //~ console.log( 'setTimeout :: clear');
           keyMove.clear();
           blurTimeOut = false;
-
         }, 900);
-
       })
       .on('keydown.autofill', e => {
+
         //~ console.log( 'keydown.autofill', e.keyCode);
         if (e.keyCode == 9 && options.autoFocus) {
+
           keyMove.select(e);
           return;
-
         }
-
       })
       .on('keypress.autofill', e => {
+
         // allowing enter to pass thru will probably submit the form, just settle here
         return (e.keyCode || e.which || e.charCode || 0) !== 13;
-
       })
       .on('keyup.autofill', function (e) {
-        if (e.shiftKey)
-          return;
+
+        if (e.shiftKey) return;
 
         if (!_.browser.isMobileDevice) {
-          if (e.keyCode == 9) {
-            return; // tab key does not trigger search
 
+          if (e.keyCode == 9) {
+
+            return; // tab key does not trigger search
           } else if (e.keyCode == 13) {
+
             keyMove.select(e);
             return;
-
           } else if (e.keyCode == 38) {
+
             keyMove.up();
             return;
-
           } else if (e.keyCode == 40) {
+
             keyMove.down();
             return;
-
           }
-
         }
 
-        if (_me.val().length < options.minLength || _me.val() == lastVal)
-          return;
+        if (_me.val().length < options.minLength || _me.val() == lastVal) return;
 
         lastVal = _me.val();
 
         let _data = {
           term: lastVal,
           iterant: ++iterant,
-
         };
 
         //~ console.log( typeof options.source);
-
         if (/^(array|object)$/.test(typeof options.source)) {
+
           keyMove
             .clear()
             .init();
 
           let rex = new RegExp(lastVal);
-          $.each(options.source, (i, el) => {
+          $.each(options.source, (i, _el) => {
+
+            let el = {
+              ...{
+                label: 'string' == typeof _el ? _el : '',
+                value: 'string' == typeof _el ? _el : '',
+              }, ..._el
+            }
+
             if (!!el.label) {
+
               if (rex.test(el.label)) {
-                $('<li class="list-group-item p-1" tabindex="-1"></li>')
-                  .append($('<div class="text-truncate"></div>').text(el.label))
+
+                $(`<li class="list-group-item p-1" tabindex="-1">
+                    <div class="text-truncate">${el.label}</div>
+                  </li>`)
                   .data('item', el)
                   .on('click', function (e) {
                     keyMove.selectitem.call(this, e);
@@ -332,27 +339,10 @@
                     keyMove.activate(this);
                   })
                   .appendTo(list);
-
+                console.log(el);
               }
-
-            } else if (rex.test(el)) {
-              $('<li class="list-group-item p-1" tabindex="-1"></li>')
-                .append($('<div class="text-truncate"></div>').text(el))
-                .data('item', {
-                  label: el,
-                  value: el
-                })
-                .on('click', function (e) {
-                  keyMove.selectitem.call(this, e);
-                })
-                .on('mouseover', function () {
-                  keyMove.activate(this);
-                })
-                .appendTo(list);
-
             }
-
-          });
+          }); // 353
 
         } else {
           setTimeout(() => {
@@ -369,32 +359,30 @@
               _li
                 .data('item', el)
                 .css('border', '1px solid dashed')
-                .on('mousedown', e => event.preventDefault() /* Prevent focus leaving field */)
+                .on('mousedown', e => e.preventDefault() /* Prevent focus leaving field */)
                 .on('click', function (e) {
-                  keyMove.selectitem.call(this, e);
 
+                  keyMove.selectitem.call(this, e);
                 })
                 .on('touchstart', function (e) {
-                  keyMove.activate(this);
-                  touchStartTimeout = window.setTimeout(() => keyMove.deactivate(this), 300);
 
+                  keyMove.activate(this);
+                  touchStartTimeout = setTimeout(() => keyMove.deactivate(this), 300);
                 })
                 .on('touchend', function (e) {
 
                   if ($(this).hasClass('active')) {
-                    if (touchStartTimeout) window.clearTimeout(touchStartTimeout);
+
+                    if (touchStartTimeout) clearTimeout(touchStartTimeout);
                     keyMove.selectitem.call(this, e);
-
                   }
-
                 })
                 .on('mouseover', function () {
-                  keyMove.activate(this);
 
+                  keyMove.activate(this);
                 });
 
               return _li;
-
             };
 
             options.source(_data, (data) => {
