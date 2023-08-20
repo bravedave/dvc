9f5f27e1ec2b4376bddfb39f1a3d37995919aee2
Dave Bray <david@brayworth.com.au>
2023-08-20 11:47:37 +1000

    refactor

--- src/bravedave/dvc/js/_brayworth_.lazyImageLoader.js
+++ src/bravedave/dvc/js/_brayworth_.lazyImageLoader.js
@@ -7,16 +7,20 @@
  *
  * */
 (_ => _.lazyImageLoader = () => {
+
   let imgStack = [];
 
   $('div[data-delayedimg="true"]').each((i, el) => {
+
     let _el = $(el);
     if (_el.visible(true)) {
+
       _el
-        .css({ 'background-image': 'url("' + _el.data('src') + '")' })
+        .css('background-image', `url("${_el.data('src')}")`)
         .data('delayedimg', false);
     }
     else {
+
       imgStack.push(_el);
     }
   });
@@ -24,36 +28,28 @@
   if (imgStack.length > 0) {
     // console.log( 'unloaded images', imgStack.length);
 
-    $(document).on('scroll', e => {
+    $(document).on('scroll.lazyLoader', e => {
+
       let unProcessed = 0;
       $.each(imgStack, (i, el) => {
+
         let _el = $(el);
         if (_el.data('delayedimg')) {
-          if (_el.visible(true)) {
-            // console.log( 'loading', _el.data('src'));
-            _el
-              .css({ 'background-image': 'url("' + _el.data('src') + '")' })
-              .data('delayedimg', false);
 
+          if (_el.visible(true)) {
+
+            _el
+              .css('background-image', `url("${_el.data('src')}")`)
+              .data('delayedimg', false);
           }
           else {
+
             unProcessed++;
-
           }
-
         }
-
       });
 
-      // console.log( 'checking unloaded images', unProcessed);
-      if (unProcessed < 1) {
-        $(document).off('scroll');
-        // console.log( 'disabled scroll check');
-
-      }
-
+      if (unProcessed < 1) $(document).off('scroll.lazyLoader');
     });
-
   }
-
 })(_brayworth_);
