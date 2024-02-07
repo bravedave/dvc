c6deda0c8665aaa7ef2b540368951d6fcd602e1e
Dave Bray <david@brayworth.com.au>
2024-02-07 16:07:36 +1000

    themes

--- src/resource/dvc-bootstrap-5.sh
+++ src/resource/dvc-bootstrap-5.sh
@@ -47,7 +47,7 @@ if [ -x "$(command -v sassc)" ]; then
     sassc --omit-map-comment -t compressed bootstrap-blue.scss $targetDir/bootstrap-blue.min.css
     echo "$me : wrote bootstrap-blue.min.css"
 
-    cat ../../bootstrap-orange.scss >>bootstrap-orange.scss
+    cat ../../bootstrap-orange.scss >bootstrap-orange.scss
     sassc --omit-map-comment -t compressed bootstrap-orange.scss $targetDir/bootstrap-orange.min.css
     echo "$me : wrote bootstrap-orange.min.css"
   else
