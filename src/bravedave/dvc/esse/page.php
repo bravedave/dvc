1f93a9d1608d7d83b824a27182e80abd089a13e4
David Bray <david@brayworth.com.au>
2024-10-20 13:00:08 +1000

    backporting

--- src/bravedave/dvc/esse/page.php
+++ src/bravedave/dvc/esse/page.php
@@ -10,7 +10,7 @@
 
 namespace bravedave\dvc\esse;
 
-use bravedave\dvc\{bs, Response};
+use bravedave\dvc\{bs, Response, userAgent};
 use config, currentUser, strings, theme;
 use Closure;
 
@@ -34,13 +34,18 @@ class page {
   public array $css = [];
   public array $late = [];
 
+  static $icon57 = '';
+  static $icon72 = '';
+  static $icon114 = '';
+
   /**
    * instantiate a page and install bootstrap
    */
-  public static function bootstrap(): self {
-    $p = new self;
+  public static function bootstrap(): static {
+    $p = new static;
 
     bs::$VERSION = config::$BOOTSTRAP_VERSION = '5';
+
     $p->meta[] = '<meta charset="utf-8">';
     $p->meta[] = '<meta name="viewport" content="width=device-width, initial-scale=1">';
 
@@ -64,6 +69,14 @@ class page {
     $p->css[] = sprintf('<link rel="stylesheet" href="%s">', $icons);
     $p->css[] = sprintf('<link rel="stylesheet" href="%s">', strings::url('assets/esse'));
     if ($_css = theme::rootFont()) $p->css[] = $_css;
+
+    $p->css['shortcut icon'] = sprintf('<link rel="shortcut icon" href="%s" />', strings::url('cms.ico'));
+    if (userAgent::isIPhone() || userAgent::isIPad()) {
+
+      if (static::$icon57) $p->css[] = sprintf('<link rel="apple-touch-icon" href="%s" />', strings::url(static::$icon57));
+      if (static::$icon72) $p->css[] = sprintf('<link rel="apple-touch-icon" sizes="72x72" href="%s" />', strings::url(static::$icon72));
+      if (static::$icon114) $p->css[] = sprintf('<link rel="apple-touch-icon" sizes="114x114" href="%s" />', strings::url(static::$icon114));
+    }
 
     return $p;
   }
@@ -122,7 +135,7 @@ class page {
 
     $this->closemainrow();
 
-    array_walk($this->late, fn ($late) => printf("\t%s\n", $late));
+    array_walk($this->late, fn($late) => printf("\t%s\n", $late));
 
     print "\n</body>\n";
 
@@ -192,9 +205,9 @@ class page {
 
     print "<head>\n";
 
-    array_walk($this->meta, fn ($meta) => printf("\t%s\n", $meta));
-    array_walk($this->css, fn ($css) => printf("\t%s\n", $css));
-    array_walk($this->scripts, fn ($scripts) => printf("\t%s\n", $scripts));
+    array_walk($this->meta, fn($meta) => printf("\t%s\n", $meta));
+    array_walk($this->css, fn($css) => printf("\t%s\n", $css));
+    array_walk($this->scripts, fn($scripts) => printf("\t%s\n", $scripts));
 
     $this->_head = true;
 
@@ -217,10 +230,10 @@ class page {
     $this->open()
       ->closeHead();
 
-    if ( $this->bodyClass ) {
+    if ($this->bodyClass) {
 
-      printf( "<body class=\"%s\">\n", $this->bodyClass);
-    }else {
+      printf("<body class=\"%s\">\n", $this->bodyClass);
+    } else {
 
       print "<body>\n";
     }
@@ -244,7 +257,7 @@ class page {
       ->mainrow()
       ->closemain();
 
-    printf( "\t\t<aside class=\"%s\" data-role=\"content-secondary\">\n", theme::secondary());
+    printf("\t\t<aside class=\"%s\" data-role=\"content-secondary\">\n", theme::secondary());
     print "\t\t\t<div class=\"sidebar pt-3 pb-5\"><!-- theme start -->\n";
 
     $this->_aside = true;
@@ -299,7 +312,7 @@ class page {
 
     $this->body();
 
-    printf( "\n\t<div class=\"%s\"><div class=\"row\">\n", $this->container);
+    printf("\n\t<div class=\"%s\"><div class=\"row\">\n", $this->container);
 
     $this->_mainrow = true;
     return $this;
