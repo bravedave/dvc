d92e49842016908056713620a394939e8704abe3
David Bray <david@brayworth.com.au>
2024-11-07 13:15:09 +1000

    modifying bootstrap 5 bug

--- src/resource/bootstrap5JS.php
+++ src/resource/bootstrap5JS.php
@@ -16,10 +16,32 @@ $minifiedPath = realpath(__DIR__ . '/../bravedave/dvc/js/bootstrap5');
 
 if ($src) {
 
-  if ( $minifiedPath) {
+  if ($minifiedPath) {
+
+    $content = file_get_contents($src);
+
+    // Remove the line with `aria-hidden` and move it after `EVENT_HIDDEN$4`
+    $updatedContent = preg_replace(
+      '/\s*this._element.setAttribute\(\'aria-hidden\', true\);\n\s*/',
+      '',
+      $content
+    );
+
+    $updatedContent = preg_replace(
+      '/(EventHandler.trigger\(this._element, EVENT_HIDDEN\$4\);)/',
+      "$1\n        this._element.setAttribute('aria-hidden', true);",
+      $updatedContent
+    );
+
+    $srcModified = $minifiedPath . '/bootstrap.bundle.js';
+
+    // Save the modified content back to the file
+    print "modifying path : $srcModified\n";
+    file_put_contents($srcModified, $updatedContent);
 
     print "target path : $minifiedPath\n";
-    $minifier = new \MatthiasMullie\Minify\JS($src);
+    // $minifier = new \MatthiasMullie\Minify\JS($src);
+    $minifier = new \MatthiasMullie\Minify\JS($srcModified);
     $minifier->minify($minifiedPath . '/bootstrap.bundle.min.js');
   } else {
 
