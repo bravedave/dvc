de85f8399fde70140f01e807e8f2e637d6205e3d
David Bray <david@brayworth.com.au>
2025-01-05 09:34:16 +1000

    PSR7

---
+++ src/bravedave/dvc/ServerRequest.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace bravedave\dvc;
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+
+abstract class ServerRequest {
+
+  public function __invoke() {
+
+    $psr17Factory = new Psr17Factory;
+
+    $creator = new ServerRequestCreator(
+      $psr17Factory, // ServerRequestFactory
+      $psr17Factory, // UriFactory
+      $psr17Factory, // UploadedFileFactory
+      $psr17Factory  // StreamFactory
+    );
+
+    return $creator->fromGlobals();
+  }
+}
