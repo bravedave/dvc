e2455d055adad9e5edd0edbd3772019224fd01a1
Dave Bray <david@brayworth.com.au>
2025-05-30 18:22:38 +1000

    escape function

---
+++ src/bravedave/dvc/functions.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ *
+ * MIT License
+ *
+*/
+
+namespace bravedave\dvc;
+
+/**
+ * bravedave\dvc\esc
+ */
+function esc(string $v) : string {
+  return htmlspecialchars($v, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', false);
+}
