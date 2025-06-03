a7ea7742b005d5d4deda5f67c67c71014abd23d0
David Bray <david@brayworth.com.au>
2025-06-03 08:17:50 +0000

    use html5 handling

--- src/bravedave/dvc/functions.php
+++ src/bravedave/dvc/functions.php
@@ -16,6 +16,7 @@ namespace bravedave\dvc;
  */
 function esc(string|null $v) : string {
   
-  if ($v) return htmlspecialchars($v, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', false);
+  if ($v) return htmlspecialchars($v, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8', false);
+  // if ($v) return htmlspecialchars($v, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', false);
   return '';
 }
