63dc8098a274382b7ce8f9e41114c3795a98220e
David Bray <david@brayworth.com.au>
2025-06-20 01:49:12 +0000

    deprecation

--- src/bravedave/dvc/strings.php
+++ src/bravedave/dvc/strings.php
@@ -1133,21 +1133,25 @@ abstract class strings {
      */
     $strStreetIndex = (string)$street;  // safety
 
-    if (is_numeric(substr($street, 0, 1))) {
-      $aStreet = explode(' ', $street);
+    if ($street) {
 
-      $no = array_shift($aStreet);
-      if (false != strpos($no, '/')) {
-        $_no = explode('/', $no);
-        $_r = array_reverse($_no);
-        $no = implode(' ', $_r);
+      if (is_numeric(substr($street, 0, 1))) {
+
+        $aStreet = explode(' ', $street);
+
+        $no = array_shift($aStreet);
+        if (false != strpos($no, '/')) {
+          $_no = explode('/', $no);
+          $_r = array_reverse($_no);
+          $no = implode(' ', $_r);
+        }
+
+        $aStreet[] = str_pad(trim((string)$no), 6, ' ', STR_PAD_LEFT);
+        $strStreetIndex = implode(' ', $aStreet);
+        if ('' == $strStreetIndex)
+          $strStreetIndex = $street;  // safety
+
       }
-
-      $aStreet[] = str_pad(trim((string)$no), 6, ' ', STR_PAD_LEFT);
-      $strStreetIndex = implode(' ', $aStreet);
-      if ('' == $strStreetIndex)
-        $strStreetIndex = $street;  // safety
-
     }
 
     return (string)$strStreetIndex;
