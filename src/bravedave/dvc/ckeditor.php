5b10c5ae5fe9b0eeb93ddbcf44d07542ee45097f
David Bray <david@brayworth.com.au>
2025-08-07 07:06:59 +0000

    add ckeditor resource

---
+++ src/bravedave/dvc/ckeditor.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * David Bray
+ * BrayWorth Pty Ltd
+ * e. david@brayworth.com.au
+ * 
+ * MIT License
+ *
+*/
+
+namespace bravedave\dvc\resources;
+
+use bravedave\dvc\Response;
+
+final class ckeditor {
+
+  public static function serve($file = '', $translation = ''): bool {
+
+    $basePath = __DIR__ . '/resources/ckeditor/ckeditor5/';
+
+    if ($file) {
+
+      $file = (string)$file;
+
+      // Serve translation files
+      if ($file === 'translations' && $translation) {
+        $translationsPath = $basePath . 'translations/';
+        $filename = $translation . '.js';
+        $filepath = $translationsPath . $filename;
+
+        if (is_file($filepath)) {
+
+          Response::serve($filepath);
+          return true;
+        }
+      }
+
+      // Serve JS, CSS, or MAP files
+      $allowedExtensions = ['js', 'css', 'map'];
+      foreach ($allowedExtensions as $ext) {
+        $filename = $file;
+        if (pathinfo($filename, PATHINFO_EXTENSION) !== $ext) {
+          $filename = $file . '.' . $ext;
+        }
+        $filepath = $basePath . $filename;
+        if (is_file($filepath)) {
+
+          Response::serve($filepath);
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
