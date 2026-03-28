6d74edce03af83edaa65e73db97dde039be662b7
David Bray <david@brayworth.com.au>
2026-03-28 14:10:38 +1000

    feat: implement _index method to handle redirection in auth controller

--- src/bravedave/dvc/controller/auth.php
+++ src/bravedave/dvc/controller/auth.php
@@ -8,15 +8,24 @@ namespace bravedave\dvc\controller;
 
 use bravedave\dvc\logger;
 use bravedave\dvc\Request;
+use bravedave\dvc\Response;
 use Controller;
 
 class auth extends Controller {
 
   protected $RequireValidation = false;
 
+  protected function _index() {
+
+    Response::redirect();
+  }
+
   protected function before() {
 
     logger::info(sprintf('<honeytrap %s> %s', Request::get()->getRemoteIP(), logger::caller()));
     parent::before();
+  }
+
+  protected function postHandler() {
   }
 }
